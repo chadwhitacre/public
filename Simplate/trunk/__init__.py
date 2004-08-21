305833845c4ac6a4d83e89d9821e549dba0a76c3
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2004-08-21 18:11:07 +0000

    polishing up Simplate.py and ZopeSimplate.py

--- Simplate/trunk/__init__.py
+++ Simplate/trunk/__init__.py
@@ -3,6 +3,9 @@ import ZopeSimplate
 
 registerDirectory('skins', globals())
 
+# Placeholder for Zope Product data
+misc_ = {}
+
 def initialize(context):
     # Import lazily, and defer initialization to the module
     import ZopeSimplate
