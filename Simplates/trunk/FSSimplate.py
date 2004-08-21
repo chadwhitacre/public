6bab2bca3407a3b0d8f2f66a0f5243a8d3163244
Chad Whitacre <chad@zetaweb.com>
2004-08-21 20:21:21 +0000

    first round on FSSimplate

--- Simplates/trunk/FSSimplate.py
+++ Simplates/trunk/FSSimplate.py
@@ -1,137 +1,219 @@
-##############################################################################
-#
-# Copyright (c) 2001 Zope Corporation and Contributors. All Rights Reserved.
-# 
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE
-# 
-##############################################################################
-"""Customizable simplate objects that come from the filesystem."""
-__version__='$Revision: 1.6.10.3 $'[11:-2]
+"""Customizable Simplates that come from the filesystem.
+"""
 
-import string, os
+from string import split, replace
+from os import stat
+import re, sys
 
-import Globals
+import Globals, Acquisition
 from DateTime import DateTime
-from AccessControl import ClassSecurityInfo
-from webdav.common import rfc1123_date
-from OFS.Image import File
-from OFS.content_types import guess_content_type
+from DocumentTemplate.DT_Util import html_quote
+from Acquisition import aq_parent
+from AccessControl import getSecurityManager, ClassSecurityInfo
+from Shared.DC.Scripts.Script import Script
+from Products.Simplates.Simplate import Simplate
+from Products.Simplates.ZopeSimplate import ZopeSimplate, Src
 
-from Products.CMFCore.utils import _dtmldir
-from Products.CMFCore.CMFCorePermissions import ViewManagementScreens, View, FTPAccess
-from Products.CMFCore.FSObject import FSObject
 from Products.CMFCore.DirectoryView import registerFileExtension, registerMetaType, expandpath
+from Products.CMFCore.CMFCorePermissions import ViewManagementScreens
+from Products.CMFCore.CMFCorePermissions import View
+from Products.CMFCore.CMFCorePermissions import FTPAccess
+from Products.CMFCore.FSObject import FSObject
+from Products.CMFCore.utils import _setCacheHeaders
+
+#xml_detect_re = re.compile('^\s*<\?xml\s+')
 
 from OFS.Cache import Cacheable
 
-class FSSimplate(FSObject):
-    """Simplate wrapper"""
-    # Note that OFS.Image.File is not a base class because it is mutable.
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 
+_marker = []  # Create a new marker object.
+
+class FSSimplate(FSObject, Script, Simplate):
+    "Wrapper for Simplate"
+     
     meta_type = 'Filesystem Simplate'
 
+    _owner = None  # Unowned
+
     manage_options=(
-        {'label':'Customize', 'action':'manage_main'},
-        ) + Cacheable.manage_options
+        (
+            {'label':'Customize', 'action':'manage_main'},
+            {'label':'Test', 'action':'ZScriptHTML_tryForm'},
+            )
+            +Cacheable.manage_options
+        ) 
 
     security = ClassSecurityInfo()
     security.declareObjectProtected(View)
 
-    def __init__(self, id, filepath, fullname=None, properties=None):
-        id = fullname or id # Use the whole filename.
-        FSObject.__init__(self, id, filepath, fullname, properties)
-
     security.declareProtected(ViewManagementScreens, 'manage_main')
-    manage_main = Globals.DTMLFile('custfile', _dtmldir)
-    content_type = 'unknown/unknown'
+    manage_main = PageTemplateFile('www/simplateFS.pt', globals())
+
+    # Declare security for unprotected Simplate methods.
+    security.declarePrivate('simplate_edit', 'write')
+
+    def __init__(self, id, filepath, fullname=None, properties=None):
+        FSObject.__init__(self, id, filepath, fullname, properties)
+        self.ZBindings_edit(self._default_bindings)
 
     def _createZODBClone(self):
-        return File(self.getId(), '', self._readFile(1))
+        """Create a ZODB (editable) equivalent of this object."""
+        obj = ZopeSimplate(self.getId(), self._text, self.content_type)
+        obj.expand = 0
+        obj.write(self.read())
+        return obj
 
-    def _get_content_type(self, file, body, id, content_type=None):
-        headers=getattr(file, 'headers', None)
-        if headers and headers.has_key('content-type'):
-            content_type=headers['content-type']
-        else:
-            if type(body) is not type(''): body=body.data
-            content_type, enc=guess_content_type(
-                getattr(file, 'filename',id), body, content_type)
-        return content_type
+#    def ZCacheable_isCachingEnabled(self):
+#        return 0
 
     def _readFile(self, reparse):
         fp = expandpath(self._filepath)
-        file = open(fp, 'rb')
-        try: data = file.read()
-        finally: file.close()
-        if reparse or self.content_type == 'unknown/unknown':
-            self.ZCacheable_invalidate()
-            self.content_type=self._get_content_type(file, data, self.id)
-        return data
+        file = open(fp, 'r')    # not 'rb', as this is a text file!
+        try: 
+            data = file.read()
+        finally: 
+            file.close()
+        if reparse:
+#            if xml_detect_re.match(data):
+#                # Smells like xml
+#                self.content_type = 'text/xml'
+#            else:
+            try:
+                del self.content_type
+            except (AttributeError, KeyError):
+                pass
+            self.write(data)
 
-    #### The following is mainly taken from OFS/File.py ###
-
-    def __str__(self):
+    security.declarePrivate('read')
+    def read(self):
+        # Tie in on an opportunity to auto-update
         self._updateFromFS()
-        return str( self._readFile( 0 ) )
+        return FSSimplate.inheritedAttribute('read')(self)
 
-    security.declareProtected(View, 'index_html')
-    def index_html(self, REQUEST, RESPONSE):
-        """
-        The default view of the contents of a File or Image.
+    ### The following is mainly taken from ZopeSimplate.py ###
 
-        Returns the contents of the file or image.  Also, sets the
-        Content-Type HTTP header to the objects content type.
-        """
+    expand = 0
+
+    func_defaults = None
+    func_code = ZopeSimplate.func_code
+    _default_bindings = ZopeSimplate._default_bindings
+
+    security.declareProtected(View, '__call__')
+
+#    def simplate_macros(self):
+#        # Tie in on an opportunity to auto-reload
+#        self._updateFromFS()
+#        return FSSimplate.inheritedAttribute('simplate_macros')(self)
+
+    def simplate_render(self, source=0, extra_context={}):
+        self._updateFromFS()  # Make sure the template has been loaded.
+        try:
+            result = FSSimplate.inheritedAttribute('simplate_render')(
+                                    self, source, extra_context
+                                    )
+            if not source:
+                _setCacheHeaders(self, extra_context)
+            return result
+
+        except RuntimeError:
+            if Globals.DevelopmentMode:
+                err = FSSimplate.inheritedAttribute( 'simplate_errors' )( self )
+                if not err:
+                    err = sys.exc_info()
+                err_type = err[0]
+                err_msg = '<pre>%s</pre>' % replace( str(err[1]), "\'", "'" )
+                msg = 'FS Simplate %s has errors: %s.<br>%s' % (
+                    self.id, err_type, html_quote(err_msg) )
+                raise RuntimeError, msg
+            else:
+                raise
+                
+    security.declarePrivate( '_SMPT_exec' )
+    _SMPT_exec = ZopeSimplate._exec
+
+    security.declarePrivate( '_exec' )
+    def _exec(self, bound_names, args, kw):
+        """Call a FSSimplate"""
+        try:
+            response = self.REQUEST.RESPONSE
+        except AttributeError:
+            response = None
+        # Read file first to get a correct content_type default value.
         self._updateFromFS()
-        data = self._readFile(0)
-        # HTTP If-Modified-Since header handling.
-        header=REQUEST.get_header('If-Modified-Since', None)
-        if header is not None:
-            header=string.split(header, ';')[0]
-            # Some proxies seem to send invalid date strings for this
-            # header. If the date string is not valid, we ignore it
-            # rather than raise an error to be generally consistent
-            # with common servers such as Apache (which can usually
-            # understand the screwy date string as a lucky side effect
-            # of the way they parse it).
-            try:    mod_since=long(DateTime(header).timeTime())
-            except: mod_since=None
-            if mod_since is not None:
-                last_mod = self._file_mod_time
-                if last_mod > 0 and last_mod <= mod_since:
-                    # Set header values since apache caching will return
-                    # Content-Length of 0 in response if size is not set here
-                    RESPONSE.setHeader('Last-Modified', rfc1123_date(last_mod))
-                    RESPONSE.setHeader('Content-Type', self.content_type)
-                    RESPONSE.setHeader('Content-Length', self.get_size())
-                    RESPONSE.setStatus(304)
-                    return ''
+        
+        if not kw.has_key('args'):
+            kw['args'] = args
+        bound_names['options'] = kw
 
-        RESPONSE.setHeader('Last-Modified', rfc1123_date(self._file_mod_time))
-        RESPONSE.setHeader('Content-Type', self.content_type)
-        RESPONSE.setHeader('Content-Length', len(data))
+        try:
+            response = self.REQUEST.RESPONSE
+            if not response.headers.has_key('content-type'):
+                response.setHeader('content-type', self.content_type)
+        except AttributeError:
+            pass
+            
+        security=getSecurityManager()
+        bound_names['user'] = security.getUser()
 
-        self.ZCacheable_set(None)
-        return data
+        # Retrieve the value from the cache.
+        keyset = None
+        if self.ZCacheable_isCachingEnabled():
+            # Prepare a cache key.
+            keyset = {
+                      # Why oh why?
+                      # All this code is cut and paste
+                      # here to make sure that we 
+                      # dont call _getContext and hence can't cache
+                      # Annoying huh?
+                      'here': self.aq_parent.getPhysicalPath(),
+                      'bound_names': bound_names}
+            result = self.ZCacheable_get(keywords=keyset)
+            if result is not None:
+                # Got a cached value.
+                return result
 
-    security.declareProtected(View, 'getContentType')
-    def getContentType(self):
-        """Get the content type of a file or image.
-
-        Returns the content type (MIME type) of a file or image.
-        """
-        self._updateFromFS()
-        return self.content_type
-
+        # Execute the template in a new security context.
+        security.addContext(self)
+        try:
+            result = self.simplate_render(extra_context=bound_names)
+            if keyset is not None:
+                # Store the result in the cache.
+                self.ZCacheable_set(result, keywords=keyset)
+            return result
+        finally:
+            security.removeContext(self)
+        
+        return result
+ 
+    # Copy over more methods
     security.declareProtected(FTPAccess, 'manage_FTPget')
-    manage_FTPget = index_html
+    manage_FTPget = ZopeSimplate.manage_FTPget
+
+    security.declareProtected(View, 'get_size')
+    get_size = ZopeSimplate.get_size
+    getSize = get_size
+
+    security.declareProtected(ViewManagementScreens, 'PrincipiaSearchSource')
+    PrincipiaSearchSource = ZopeSimplate.PrincipiaSearchSource
+
+    security.declareProtected(ViewManagementScreens, 'document_src')
+    document_src = ZopeSimplate.document_src
+
+    simplate_getContext = ZopeSimplate.simplate_getContext
+
+    ZScriptHTML_tryParams = ZopeSimplate.ZScriptHTML_tryParams
+
+
+#s = Src()
+#setattr(FSSimplate, 'source.xml', s)
+#setattr(FSSimplate, 'source.html', s)
+#del s
 
 Globals.InitializeClass(FSSimplate)
 
+registerFileExtension('smpt', FSSimplate)
 registerFileExtension('smpl', FSSimplate)
+registerFileExtension('spt', FSSimplate)
 registerMetaType('Simplate', FSSimplate)
+
