0c8a882889c5d5698bb0c8db719826318ff7a9e2
Chad Whitacre <chad@zetaweb.com>
2004-08-23 15:38:12 +0000

    remember to import FSSimplate

--- Simplates/trunk/__init__.py
+++ Simplates/trunk/__init__.py
@@ -1,9 +1,11 @@
+import FSSimplate
+
 from Products.CMFCore.DirectoryView import registerDirectory
 registerDirectory('skins', globals())
 
 
 # Placeholder for Zope Product data
-misc_ = {}
+#misc_ = {}
 
 def initialize(context):
     # Import lazily, and defer initialization to the module
