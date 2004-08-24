e6bf024537f7ffd6185f3fabf3f31410c6a0422c
Chad Whitacre <chad@zetaweb.com>
2004-08-24 12:02:38 +0000

    initial refactor

--- QIHelpers/trunk/config.py
+++
@@ -1,7 +0,0 @@
-import os
-PROJECT_NAME = 'ZetaUtils'
-SKINS_DIR = 'skins'
-ihome = os.environ.get('INSTANCE_HOME', '')
-SKINS_PATH = os.path.join(ihome,'Products',PROJECT_NAME,SKINS_DIR)
-
-GLOBALS = globals()
