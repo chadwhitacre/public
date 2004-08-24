18fe9c239e593dafc2e1af7ef7c2c7b9ff38eba5
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2004-08-24 12:02:38 +0000

    initial refactor

--- QIHelpers/trunk/__init__.py
+++ QIHelpers/trunk/__init__.py
@@ -1,19 +1,9 @@
-from AccessControl import ModuleSecurityInfo
-from Products.CMFCore.DirectoryView import registerDirectory
-from config import *
-from pprint import pformat
-from utils import *
+from Skins      import  installSkins, uninstallSkins
+from Properties import  installProperties
+from Catalog    import  installIndices, uninstallIndices, \
+                        installMetadata, uninstallMetadata
 
-import Skinstall
-import FolderTypes
-import CatalogInstall
-
-security = ModuleSecurityInfo()
-security.declarePublic('pformat')
-security.declarePublic('mxNow','mxToday','mxDateTime','mxRelativeDateTime', 'mx_to_zope','zope_to_mx')
-security.declarePublic('lists_to_csv','csv_to_lists')
-security.declarePublic('request_dict')
-security.apply(globals())
-
-
-registerDirectory(SKINS_DIR, GLOBALS)
+try:
+    from Products.Archetypes.Extensions.utils import installTypes
+except:
+    pass
