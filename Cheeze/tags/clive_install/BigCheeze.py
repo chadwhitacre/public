71642cce1f9ea30db3e35ab5a513f96be9733ee2
beren1hand <beren1hand@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2004-09-08 16:27:14 +0000

    this is the version that is going to be installed on clive

---
+++ Cheeze/tags/clive_install/BigCheeze.py
@@ -0,0 +1,392 @@
+try:
+    from Products.Cheeze.vh_utils import _vhosts_update, \
+                                         _vhosts_recreate, \
+                                         _vhosts_get
+    vhosting = 1
+except:
+    # we are not on unix :/
+    vhosting = 0
+#from Products.ZetaUtils import compare_domains, pformat, index_sort
+from AccessControl import ClassSecurityInfo
+
+from Acquisition import Implicit
+from Globals import Persistent
+from AccessControl.Role import RoleManager
+from OFS.PropertyManager import PropertyManager
+from OFS.SimpleItem import Item
+
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from Globals import DTMLFile, ImageFile
+import os
+from os.path import join
+
+
+from CheezeError import CheezeError
+from ZopeManager import ZopeManager
+from ApacheVHostManager import ApacheVHostManager
+from DNSManager import DNSManager
+
+
+from pprint import pformat as Ipformat
+from utils import *
+
+class BigCheeze(Implicit, Persistent, \
+                PropertyManager, Item, \
+                ZopeManager, DNSManager,  ApacheVHostManager):
+
+
+    security = ClassSecurityInfo()
+
+    id = 'Cheeze'
+    title = 'Cheap Zopes :-)'
+    meta_type= 'Big Cheeze'
+
+    instance_root = skel_root = vhost_db = dns_file = port_range = port_list=''
+    production_mode=0
+
+    vhosting = 0 # do we still need this?
+
+    BigCheeze_manage_options = (
+        {'label':'Zopes',           'action':'manage_zopes',    },
+        {'label':'Domains',         'action':'manage_domains',  },
+        {'label':'Documentation',   'action':'manage_doc',      },
+                                )
+
+    manage_options = BigCheeze_manage_options \
+                   + PropertyManager.manage_options \
+                   + RoleManager.manage_options \
+                   + Item.manage_options
+
+    _properties=(
+        {'id'   :'title',        'type' :'string','value':'','mode': 'w',},
+        {'id'   :'production_mode',        'type' :'boolean','value':0,'mode': 'w',},
+        {'id'   :'instance_root','type' :'string','value':'','mode': 'w',},
+        {'id'   :'skel_root',    'type' :'string','value':'','mode': 'w',},
+        {'id'   :'vhost_db',     'type' :'string','value':'','mode': 'w',},
+        {'id'   :'dns_file',     'type' :'string','value':'','mode': 'w',},
+        {'id'   :'port_range',   'type' :'string','value':'','mode': 'w',},
+        {'id'   :'port_list',    'type' :'string','value':'','mode': 'w',},
+                )
+
+    def __init__(self, id):
+        self.id = str(id)
+
+    ##
+    #   temp
+    ##
+    def pformat(self,object):
+        return Ipformat(object)
+
+
+    ##
+    # documentation
+    ##
+
+    dependencies = DTMLFile('doc/dependencies.txt',globals())
+
+    manage_doc = PageTemplateFile('www/manage_doc.pt',globals())
+
+    style_doc  = DTMLFile('www/style_doc.css',globals())
+
+    def explain(self):
+        """  """
+        return 'Cheap Zopes :-)'
+
+    ##
+    # A couple of functions to help us figure out how to act
+    ##
+
+    def managesInstances(self):
+        if self.instance_root:
+            return 1
+        else:
+            return 0
+
+    def managesVhosting(self):
+        if self.vhost_db:
+            return 1
+        else:
+            return 0
+
+    def mode(self):
+        managesInstances = self.managesInstances()
+        managesVhosting = self.managesVhosting()
+        if not(managesInstances or managesVhosting):
+            return 0
+        elif managesInstances and not managesVhosting:
+            return 1
+        elif not managesInstances and managesVhosting:
+            return 2
+        elif managesInstances and managesVhosting:
+            return 3
+
+    def friendly_mode(self):
+        friendlies={
+        0:'''Just installed''',
+        1:'''Manages zope instances''',
+        2:'''Manages vhosting db''',
+        3:'''Manages zope instances and vhosting db'''
+        }
+        return friendlies[self.mode()]
+
+
+    ##
+    # Zope mgmt
+    ##
+
+    manage_zopes = PageTemplateFile('www/manage_zopes.pt',globals())
+
+    security.declareProtected('Manage Big Cheeze', 'zope_add'),
+    def zope_add(self):
+        """ add a zope instance """
+        mode = self.mode()
+        if mode in [1,3]:
+            ZopeManager._zope_add(self)
+            if mode==3:
+                self.fs_db_sync()
+        elif mode==2:
+            ApacheVHostManager._zope_add(self)
+        return self.REQUEST.RESPONSE.redirect('manage')
+
+    security.declareProtected('Manage Big Cheeze', 'zope_edit'),
+    def zope_edit(self):
+        """ edit a zope instance """
+        if self.production_mode:
+            raise CheezeError, 'Cannot edit instances in production mode'
+        mode = self.mode()
+        if mode in [1,3]:
+            ZopeManager._zope_edit(self)
+            if mode==3:
+                self.fs_db_sync()
+        elif mode==2:
+            ApacheVHostManager._zope_edit(self)
+
+        return self.REQUEST.RESPONSE.redirect('manage')
+
+    security.declareProtected('Manage Big Cheeze', 'zope_remove'),
+    def zope_remove(self):
+        """ remove a zope instance """
+        mode = self.mode()
+        if mode in [1,3]:
+            ZopeManager._zope_remove(self)
+            if mode==3:
+                self.fs_db_sync()
+        elif mode==2:
+            ApacheVHostManager._zope_remove(self)
+        return self.REQUEST.RESPONSE.redirect('manage')
+
+    def zope_info(self):
+        '''just passes stuff over to manage_zopes'''
+        info = {}
+        zopes = []
+        mode = self.mode()
+        for zope_id in self.zope_ids_list():
+            name, port = self.zope_info_get(zope_id)
+            zope_info = {
+                'name':name,
+                'port':port,
+                'id':zope_id,
+                'canonical':zope_id+'.zetaserver.com',
+            }
+            zopes.append(zope_info)
+
+        #if mode==2: zopes = []
+        info['zopes'] = zopes
+        return info
+
+    def zopes_filter_set(self, regex):
+        """ takes a regex filter and sets it in a cookie """
+        r = self.REQUEST.RESPONSE
+        r.setCookie('zopes_filter', regex,
+                    expires='Wed, 19 Feb 2020 14:28:00 GMT')
+        r.redirect('manage_zopes')
+
+    def zopes_filter_get(self):
+        """ returns a regex from a form or a cookie """
+        form = self.REQUEST.form.get('regex','')
+        cookie = self.REQUEST.cookies.get('zopes_filter','')
+        if form != '':
+            return form
+        else:
+            return cookie
+
+
+
+    ##
+    # Domain mgmt
+    ##
+
+    manage_domains = PageTemplateFile('www/manage_domains.pt',globals())
+
+    def domain_add(self):
+        """handles adding domains"""
+        self._domain_add()
+        return self.REQUEST.RESPONSE.redirect('manage_domains')
+
+    def domain_edit(self):
+        """ add a domain instance """
+        self._domain_edit()
+        return self.REQUEST.RESPONSE.redirect('manage_domains')
+
+    def domain_remove(self):
+        """handles removing domains"""
+        self._domain_remove()
+        return self.REQUEST.RESPONSE.redirect('manage_domains')
+
+    def domains_info(self):
+        """ populates the domains pt """
+        vhosts = self.domains_list()
+        index_sort(vhosts,0,compare_domains)
+        info = {}
+
+        alias_map= {}
+        for domain, port in vhosts:
+            domain_list = alias_map.get(port,[])
+            domain_list.append(domain)
+            alias_map[port]=domain_list
+        #ok so if we have an instance_root, then we wanna sync up with the filesystem
+        #otherwise, we just store it in there and count on humans to make sure it actually works
+        zopes = [(z,z.split('_')[-1]) for z in self.zope_ids_list()]
+        info['zopes'] = zopes
+
+        zope_map = dict([(zport, zname) for zname, zport in zopes])
+
+        canon_map = dict([(zport, zname+'.zetaserver.com') for zname, zport in zopes])
+
+        domains =[]
+        for domain, port in vhosts:
+            try:
+                aliases = alias_map[port][:]
+                aliases.remove(domain)
+                domain_info = {
+                    'name':domain,
+                    'port':port,
+                    'zope':zope_map[port],
+                    'canonical':canon_map[port],
+                    'aliases':aliases,
+                }
+
+            except KeyError:
+                domain_info = {
+                    'name':domain,
+                    'port':port,
+                    'zope':'ORPHANED',
+                    'canonical':'',
+                    'aliases':[],
+                }
+            domains.append(domain_info)
+        info['domains']=domains
+        return info
+
+
+
+    ##
+    # helpers
+    ##
+
+    def _setPropValue(self, id, value):
+        """ intercept from PropertyManager so we can do validation """
+        if   id == 'instance_root':
+            self._set_instance_root(value)
+        elif id == 'skel_root':
+            self._set_skel_root(value)
+        elif id == 'vhost_db':
+            self._set_vhost_db(value)
+        elif id == 'port_range':
+            self._set_port_range(value)
+        else:
+            PropertyManager._setPropValue(self, id, value)
+
+    def _set_instance_root(self, instance_root):
+        """ validate and set the instance root """
+        if instance_root == '':
+            PropertyManager._setPropValue(self, 'instance_root', '')
+        elif not os.path.exists(instance_root):
+            raise CheezeError, "Proposed instance root " \
+                             + "'%s' does not exist" % instance_root
+        elif not os.path.isdir(instance_root):
+            raise CheezeError, "Proposed instance root " \
+                             + "'%s' " % instance_root \
+                             + "does not point to a directory"
+        else:
+            clean_path = self._scrub_path(instance_root)
+            PropertyManager._setPropValue(self,
+                                          'instance_root',
+                                          clean_path)
+
+    def _set_skel_root(self, skel_root):
+        """ validate and set the skel root """
+        if skel_root == '':
+            PropertyManager._setPropValue(self, 'skel_root', '')
+        elif not os.path.exists(skel_root):
+            raise CheezeError, "Proposed skel root " \
+                             + "'%s' does not exist" % skel_root
+        elif not os.path.isdir(skel_root):
+            raise CheezeError, "Proposed skel root '%s' " % skel_root \
+                             + "does not point to a directory"
+        else:
+            clean_path = self._scrub_path(skel_root)
+            PropertyManager._setPropValue(self, 'skel_root', clean_path)
+
+    def _set_port_range(self, port_range):
+        if port_range == '':
+            PropertyManager._setPropValue(self, 'port_range', '')
+        else:
+            self._ports_list(port_range) # smoke it!
+            PropertyManager._setPropValue(self, 'port_range', port_range)
+
+    def _set_vhost_db(self, vhost_db):
+        """ validate and set the vhost db"""
+        from whichdb import whichdb
+        if vhost_db == '':
+            PropertyManager._setPropValue(self, 'vhost_db', '')
+        elif whichdb(vhost_db) is None or whichdb(vhost_db) == '':
+            raise CheezeError, "vhost_db must point to a valid dbm file"
+        else:
+            clean_path = self._scrub_path(vhost_db)
+            PropertyManager._setPropValue(self, 'vhost_db', clean_path)
+
+
+    def _scrub_path(self, p):
+        """ given a valid path, return a clean path """
+        p = os.path.normpath(p)
+        p = os.path.normcase(p)
+        return p
+
+
+    ##
+    # presentation helpers
+    ##
+
+    style           = DTMLFile('www/style.css',globals())
+
+    image_delete    = ImageFile('www/delete.png',globals(),)
+
+    image_save      = ImageFile('www/save.png',globals(),)
+
+    image_zopes     = ImageFile('www/zopes.png',globals(),)
+
+    manage = manage_main = manage_zopes
+
+
+
+##
+# Product addition and registration
+##
+
+manage_add = PageTemplateFile('www/manage_add.pt', globals())
+
+def manage_addBigCheeze(self, id, REQUEST=None):
+    """  """
+    self._setObject(id, BigCheeze(id))
+
+    if REQUEST is not None:
+        return self.manage_main(self, REQUEST, update_menu=1)
+
+def initialize(context):
+    context.registerClass(
+        BigCheeze,
+        permission='Add Big Cheeze',
+        constructors=(manage_add, manage_addBigCheeze),
+        icon='www/big_cheeze.png',
+        )
---
+++ Cheeze/tags/clive_install/ZopeManager.py
@@ -0,0 +1,314 @@
+from AccessControl import ClassSecurityInfo
+import os
+from os.path import join
+from CheezeError import CheezeError
+
+class ZopeManager:
+    """ provides functionality to manage Zope instances """
+
+    security = ClassSecurityInfo()
+
+    ##
+    # info providers - these can be used in mgmt zpt's
+    ##
+
+    security.declareProtected('Manage Big Cheeze', 'zope_ids_list',
+                                                   'skel_list',
+                                                   'zope_info_get',
+                                                   'ports_list_available',
+                                                   )
+
+    def zope_ids_list(self):
+        """ return a list of available zopes, optionally constrained by regex """
+        regex = self.zopes_filter_get()
+        mode = self.mode()
+        if mode in [1,3]:
+            all_ids = result = os.listdir(self.instance_root)
+            if regex != '':
+                try:
+                    import re
+                    result = []
+                    for zope_id in all_ids:
+                        str_to_search = self._zope_search_str(zope_id)
+                        if re.search(regex, str_to_search) is not None:
+                               result.append(zope_id)
+                except:
+                    import sys
+                    self.zopes_filter_set("regex < %s > has an " % regex \
+                                        + "error: %s" % sys.exc_info()[1])
+            ids = result
+        elif mode==2:
+            pattern = '.zetaserver.com'
+            ids = [dname.replace(pattern,'') \
+                   for dname, port in self.canonical_names_list() \
+                   if dname.count(pattern)]
+        ids.sort()
+        return ids
+
+    def _zope_search_str(self, zope_id):
+        """ return a string to be used in regex filtering """
+        # we are not implementing header: here since we only have two columns
+        # and they are of different types
+        return '%s\n%s' % self.zope_info_get(zope_id)
+
+    def zopes_list(self):
+        """ return a list of name, port tuples """
+        return [zope_info_get(z) for z in self.zope_ids_list()]
+
+    def zope_info_get(self, zope_id):
+        """ given a zope_id, return (name, port number) """
+        port = zope_id.split('_')[-1]
+        name = ''.join(zope_id.split('_')[0:-1])
+        return (name, port)
+
+    def skel_list(self):
+        """ return a list of available zope skel """
+        if self.skel_root == '':
+            return None
+        else:
+            return os.listdir(self.skel_root)
+
+    def ports_list_available(self, include = None):
+        """ return a list of available ports,
+            including an optional arbitrary port """
+        mode = self.mode()
+        if include is not None:
+            try:
+                include = int(include)
+                if include < 1:
+                    raise CheezeError
+            except:
+                raise CheezeError, "include must be a positive integer"
+        all_ports = self._ports_list()
+        if all_ports:
+            for zope_id in self.zope_ids_list():
+                port = int(self.zope_info_get(zope_id)[1])
+                if (port < 1 or port != include) and port in all_ports:
+                    all_ports.remove(port)
+                if include not in all_ports and include is not None:
+                    all_ports.append(include)
+            return all_ports
+        else:
+            return False
+
+    ##
+    # heavy lifters - these are only used by BigCheeze wrappers
+    ##
+
+    def _zope_add(self):
+        """ create a new zope instance """
+        form = self.REQUEST.form
+        name = form['name']
+        skel = form['skel']
+        port = form['port']
+        # initialize kw
+        kw = self._initialize_kw()
+
+        # import things
+        import sys
+        bin = join(kw['ZOPE_HOME'], 'bin')
+        sys.path.append(bin)
+        import copyzopeskel
+        import mkzopeinstance
+
+        # set skelsrc
+        if skel == '|stock|':
+            # default to using stock Zope skeleton source
+            # NB: kw['HTTP_PORT'] will be meaningless!
+            skelsrc = join(kw['ZOPE_HOME'], 'skel')
+        else:
+            skelsrc = join(self.skel_root, skel)
+
+        # set port number
+        if port == '':
+            port = 80
+        kw['HTTP_PORT'] = str(port)
+        kw['FTP_PORT'] = str(int(port)+1)
+
+        # set skeltarget
+        zope_id = self._zope_id_make(name,port)
+        kw['INSTANCE_HOME'] = skeltarget \
+                            = join(self.instance_root,zope_id)
+
+        # now make the zope!
+        copyzopeskel.copyskel(skelsrc, skeltarget, None, None, **kw)
+
+        # and finally create the inituser
+        # username:password are hardcoded for now
+        inituser = join(kw['INSTANCE_HOME'], "inituser")
+        mkzopeinstance.write_inituser(inituser, 'admin', 'jesus')
+
+    def _zope_edit(self):
+        form = self.REQUEST.form
+        old_name = form['old_name']
+        new_name = form['new_name']
+        old_port = form['old_port']
+        new_port = form['new_port']
+
+        old_zope_id = self._zope_id_make(old_name, old_port)
+        new_zope_id = self._zope_id_make(new_name, new_port)
+        if old_zope_id != new_zope_id:
+            self._zope_id_set(old_zope_id, new_zope_id)
+        if old_port != new_port:
+            self._port_set(new_zope_id, old_port)
+
+    def _zope_remove(self):
+        """ grabs instance name from form posts, deletes it"""
+        zope_id = self.REQUEST.form['zope_id']
+        if self.production_mode:
+            raise CheezeError, 'Cannot delete instances in production mode'
+
+        top = join(self.instance_root, zope_id)
+        #raise 'top', top
+        for root, dirs, files in os.walk(top, topdown=False):
+            for name in files:
+                os.remove(join(root, name))
+            for name in dirs:
+                os.rmdir(join(root, name))
+        os.rmdir(top)
+
+    def _zope_id_set(self, old_id, new_id):
+        """ rename a zope instance """
+
+        old_path = join(self.instance_root, old_id)
+        new_path = join(self.instance_root, new_id)
+
+        # rename the directory
+        # do this first since it is more failure-prone than file changes below
+        os.rename(old_path, new_path)
+
+        # update a few files
+        bin = join(new_path, 'bin')
+        etc = join(new_path, 'etc')
+
+        paths =  [join(bin, fn) for fn in os.listdir(bin)]
+        paths += [join(etc, fn) for fn in os.listdir(etc)]
+
+        for path in paths:
+            txt = file(path).read()
+            txt = txt.replace(old_path,new_path)
+            file(path, 'w').write(txt)
+
+    def _port_set(self, zope_id, old_port):
+        """ given a new zope_id and the old port number, reset the port number """
+
+        # get the configuration file
+        conf = join(self.instance_root, zope_id, 'etc/zope.conf')
+
+        # get the new port number from the zope_id
+        new_port = self.zope_info_get(zope_id)[1]
+
+        # set the port definitions for zope.conf
+        port_def = '%%define HTTP_PORT %s'
+        old_port_def = port_def % old_port
+        new_port_def = port_def % new_port
+
+        # now do the replacement
+        txt = file(conf).read()
+        txt = txt.replace(old_port_def, new_port_def)
+        file(conf, 'w').write(txt)
+
+
+
+
+
+
+    ##
+    # helpers
+    ##
+
+    def _zope_id_make(self, name, port):
+        return '_'.join((str(name), str(port)))
+
+    def _initialize_kw(self):
+        """ return initial kw for use in copyzopeskel """
+        import sys
+
+        ### BEGIN COPY FROM mkzopeinstance.py ###
+
+        # we need to distinguish between python.exe and pythonw.exe under
+        # Windows in order to make Zope run using python.exe when run in a
+        # console window and pythonw.exe when run as a service, so we do a bit
+        # of sniffing here.
+        psplit = os.path.split(sys.executable)
+        exedir = join(*psplit[:-1])
+        pythonexe = join(exedir, 'python.exe')
+        pythonwexe = join(exedir, 'pythonw.exe')
+
+        if ( os.path.isfile(pythonwexe) and os.path.isfile(pythonexe) and
+             (sys.executable in [pythonwexe, pythonexe]) ):
+            # we're using a Windows build with both python.exe and pythonw.exe
+            # in the same directory
+            PYTHON = pythonexe
+            PYTHONW = pythonwexe
+        else:
+            # we're on UNIX or we have a nonstandard Windows setup
+            PYTHON = PYTHONW = sys.executable
+
+        ### END COPY FROM mkzopeinstance.py ###
+
+        softwarehome = os.environ.get('SOFTWARE_HOME', '')
+        zopehome     = os.environ.get('ZOPE_HOME', '')
+
+        return {"PYTHON"        : PYTHON,
+                "PYTHONW"       : PYTHONW,
+                "SOFTWARE_HOME" : softwarehome,
+                "ZOPE_HOME"     : zopehome,
+                }
+
+
+    def _instance_ports_get(self):
+        """get the port of this zope instance so it can be excluded from lists of
+        available ports"""
+        ports = []
+        for server in self.Control_Panel.getServers():
+            name, port = server
+            if port.count('Port:'):
+                port = int(port.replace('Port:',''))
+                ports.append(port)
+        return ports
+
+
+    def test_func(self):
+        '''teehee'''
+        output = self._ports_list()
+        return self.pformat(output)
+
+
+    def _ports_list(self, port_range = None, port_list = None):
+        """ list all possible ports, if applicable """
+
+        if port_range is None:
+            port_range = self.port_range
+        port_range = port_range.strip()
+
+        if port_list is None:
+            port_list = self.port_list
+        port_list = port_list.strip()
+
+        ###
+        ## the tuple format seems unnecesary here. how bout just a comma
+        ## separated list
+        ###
+        ports = []
+        if port_range:
+
+            try:
+                parts = [int(n) for n in port_range.split(',') if n]
+            except ValueError:
+                raise CheezeError, "port_range must contain only integers"
+
+            if len(parts) not in [2,3]:
+                raise CheezeError, "port_range needs a comma separated list of length 2 or 3"
+
+            ports.extend(range(*parts))
+
+        if port_list:
+            parts = [int(n) for n in port_list.split(',') if n]
+            ports.extend(parts)
+
+        for port in self._instance_ports_get():
+                if port in ports:
+                    ports.remove(port)
+        ports.sort()
+        return ports
