360f68b64fb89c0a333308977f02bd1f20a3ee9c
Chad Whitacre <chad@zetaweb.com>
2005-01-20 21:28:06 +0000

    branching for 0.2 production branch

---
+++ Porter/branches/0.2/start.py
@@ -0,0 +1,14 @@
+import sys
+from os.path import join
+from Porter import Porter
+from ConfigParser import RawConfigParser
+
+def main():
+    c = Porter()
+    try:
+        c.cmdloop()
+    except KeyboardInterrupt:
+        c.onecmd("EOF")
+
+if __name__ == '__main__':
+    main()
