be28d59cd91ed691f107c82b587183cabe298b2d
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2005-01-21 20:22:49 +0000

    successfully working with Fellow; listings regactored; not tests yet

--- Porter/branches/whit537-fellow/Porter.py
+++ Porter/branches/whit537-fellow/Porter.py
@@ -7,8 +7,14 @@ index that we use to present aliases for convenience. Then whenever we do one of
 
 """
 
-import cmd, datetime, dbm, os, shutil, sys
+import cmd, datetime, dbm, os, shutil, sys, httplib
+from SocketServer import socket # only using socket.error
+from popen2 import Popen3
+from base64 import b64decode
+from cPickle import loads
 from os.path import join
+
+__version__ = 0.3
 
 class PorterError(RuntimeError):
     """ error class for porter """
@@ -24,9 +30,14 @@ class Porter(cmd.Cmd):
         self.var = join(INSTANCE_HOME, 'var')
         self.db_path = join(self.var, "rewrite")
 
-        # read our data from storage into self.domains
-        #  we also populate the index self.aliases in this method
-        self._read_from_disk()
+        # we maintain five indices that are initialized on startup:
+        #  self.aliases   - one-to-many mapping of server:port to domains
+        #  self.domains   - one-to-many mapping of domains to server:port
+        # the following three are not changed by porter (they are read-only)
+        #  self.codenames - one-to-many mapping of server:ports to codenames
+        #  self.servers   - mapping of hostnames to (status, numsites) tuples
+        #  self.websites  - mapping of website ids to port numbers
+        self._initialize_data()
 
         # let our superclass have its way too
         cmd.Cmd.__init__(self, *args, **kw)
@@ -39,11 +50,11 @@ class Porter(cmd.Cmd):
 
         self.intro = """
 #------------------------------------------------------------------------#
-#  Porter v0.1 (c)2004-%s Zeta Design & Development <www.zetaweb.com>  #
+#  Porter v%s (c)2004-%s Zeta Design & Development <www.zetaweb.com>  #
 #------------------------------------------------------------------------#
 
 You are currently managing %s %s. Type ? for help.
-        """ % (year, num, word)
+        """ % (__version__, year, num, word)
         self.prompt = 'porter> '
 
     ##
@@ -65,7 +76,7 @@ http server architecture. For more on Cambridge ... um, talk to Chad. ;^)
 Commands available:
 
     ls -- list available domains
-          OPTIONS: -l/--long, -i/--info, -r/--raw
+          OPTIONS: -i/--info, -l/--long, -r/--raw, -s/-servers -w/--websites
           ARGS: With no options or with the -l option, ls takes an optional
                 argument. If you pass in this argument, Porter only lists
                 domains that begin with that value. So for example, 'ls zeta'
@@ -90,23 +101,8 @@ Commands available:
     def _complete_domains(self, text, line, begidx, endidx):
         return [d for d in self.domains.keys() if d.startswith(text)]
 
-    def _complete_servers(self, text, line, begidx, endidx):
-        def ping(hostname):
-            """ given a hostname, return a boolean """
-            # ping it on port 8000, return True unless ping fails
-            return True
-
-        etc_hosts = file('/etc/hosts')
-        servers= []
-        for hostname in etc_hosts:
-            if ping(hostname):
-                # info = http://hostname:8000/fellow
-                # if info is valid:
-                servers.append(hostname)
-        return [s for s in servers if s.startswith(text)]
-
-    def _complete_ports(self, text, line, begidx, endidx):
-        return [d for d in self.domains.keys() if d.startswith(text)]
+    def _complete_websites(self, text, line, begidx, endidx):
+        return [s for s in self.websites.keys() if s.startswith(text)]
 
     # this is the one that actually gets called
     def complete_smart(self, text, line, begidx, endidx):
@@ -166,68 +162,137 @@ Commands available:
     # Read
     ##
 
-    complete_ls = complete_smart
+    complete_ls = _complete_domains
 
     def do_ls(self, inStr=''):
-        """ print out a list of the domains we are managing """
-        opts, args = self._parse_inStr(inStr)
-        domains = self.domains.keys()
-        if ('i' in opts) or ('info' in opts):
+        """ list various things we are managing """
+
+        ##
+        # first define our various listing methods
+        ##
+
+        def _list_domains(args):
+            """ marshall and return domains list """
+            domains = self.domains.keys()
+            domains.sort(self._domain_cmp)
+            if args:
+                domains = [d for d in domains if d.startswith(args[0])]
+            return domains
+
+        def list_domains_short(args):
+            """ format and output a basic domains listing """
+            # columnize is an undocumented method in cmd.py
+            domains = _list_domains(args)
+            if domains: # otherwise columnize gives us '<empty>'
+                self.columnize(domains, displaywidth=79)
+
+        def list_domains_long(args):
+            """ format and output an extended domains listing """
+            domains = _list_domains(args)
+            if domains:
+                header = """
+DOMAIN NAME               WEBSITE                   ALIASES
+%s""" % (self.ruler*79,)
+                print >> self.stdout, header
+                for domain in domains:
+                    server_port = self.domains[domain]
+                    aliases = self.aliases[self.domains[domain]][:]
+                    aliases.remove(domain) # don't list ourselves in
+                                           # aliases
+
+                    # format our fields
+                    domain  = domain.ljust(24)[:24]
+                    website = self.codenames.get(server_port)
+                    if website is None:
+                        website = 'MISSING@' + server_port
+                    website = website.ljust(24)[:24]
+                    if aliases: alias = aliases.pop(0)[:24]
+                    else:       alias = ''
+
+                    # build and output our record
+                    record = "%s  %s  %s" % (domain, website, alias)
+                    print >> self.stdout, record
+                    for alias in aliases:
+                        print >> self.stdout, ' '*50 + alias
+                print >> self.stdout # newline
+
+        def list_info(ignored):
+            """ give some basic stats """
             num = len(self.domains)
             if num == 1: word = 'domain'
             if num <> 1: word = 'domains'
             print >> self.stdout, "You are currently managing " +\
                                   "%s %s." % (num, word)
-            return
-        if len(domains) > 0: # otherwise columnize gives us "<empty>"
 
-            # TODO: this might be big enough to refactor into dict switch
-            # notation
-
-            if ('r' in opts) or ('raw' in opts):
-                print >> self.stdout, """
+        def list_raw(ignored):
+            """ show exactly what is in our dbm """
+            print >> self.stdout, """
 KEY                           VALUE\n%s""" % (self.ruler*60,)
-                raw = dbm.open(self.db_path,'r')
-                for key in dict(raw):
-                    print >> self.stdout, "%s  %s" % (key.ljust(28),
-                                                      raw[key].ljust(28))
-                print >> self.stdout
-                raw.close()
-            else:
-                # massage our list of domains
-                domains.sort(self._domain_cmp)
-                if args:
-                    domains = filter(lambda d: d.startswith(args[0]), domains)
+            raw = dbm.open(self.db_path,'r')
+            for key in dict(raw):
+                print >> self.stdout, "%s  %s" % (key.ljust(28),
+                                                  raw[key].ljust(28))
+            print >> self.stdout # newline
+            raw.close()
 
-                if ('l' in opts) or ('long' in opts):
-                    header = """
-DOMAIN NAME                   SERVER        PORT  ALIASES
+        def list_servers(ignored):
+            """ show what servers we know about """
+            header = """
+SERVER                          # WEBSITES      STATUS
 %s""" % (self.ruler*79,)
-                    print >> self.stdout, header
-                    for domain in domains:
-                        server, portnum = self.domains[domain].split(':')
-                        aliases = self.aliases[self.domains[domain]][:]
-                        aliases.remove(domain) # don't list ourselves in
-                                               # aliases
+            print >> self.stdout, header
+            servers = self.servers.keys()
+            if servers:
+                servers.sort()
+                for server in servers:
 
-                        # format our fields
-                        domain  = domain.ljust(28)[:28]
-                        server  = server.ljust(12)[:12]
-                        portnum = str(portnum).rjust(4)
-                        if aliases: alias = aliases.pop(0)[:28]
-                        else:       alias = ''
+                    numsites, status = self.servers[server]
 
-                        # build and output our record
-                        record = "%s  %s  %s  %s" % (domain, server,
-                                                     portnum, alias)
-                        print >> self.stdout, record
-                        for alias in aliases:
-                            print >> self.stdout, ' '*50 + alias
-                    print >> self.stdout # newline
+                    # format our fields
+                    server   = server.ljust(30)[:30]
+                    numsites = str(numsites).rjust(12)[:12]
+                    status   = status.ljust(15)[:15]
 
-                else:
-                    # columnize is an undocumented method in cmd.py
-                    self.columnize(domains, displaywidth=79)
+                    # build and output our record
+                    record = "%s  %s  %s" % (server, numsites, status)
+                    print >> self.stdout, record
+                print >> self.stdout # newline
+
+        def list_websites(ignored):
+            """ show what websites we know about """
+            websites = self.websites.keys()
+            if websites:
+                websites.sort()
+                self.columnize(websites, displaywidth=79)
+
+        ##
+        # and then pick one
+        ##
+
+        opts, args = self._parse_inStr(inStr)
+
+        default = list_domains_short
+
+        options = { 'd'         : list_domains_short
+                  , 'domains'   : list_domains_short
+                  , 'i'         : list_info
+                  , 'info'      : list_info
+                  , 'l'         : list_domains_long
+                  , 'long'      : list_domains_long
+                  , 'r'         : list_raw
+                  , 'raw'       : list_raw
+                  , 's'         : list_servers
+                  , 'servers'   : list_servers
+                  , 'w'         : list_websites
+                  , 'websites'  : list_websites
+                   }
+
+        if opts:
+            list_func = options.get(opts[0], default)
+        else:
+            list_func = default
+        list_func(args)
+
 
 
     ##
@@ -257,8 +322,8 @@ DOMAIN NAME                   SERVER        PORT  ALIASES
     # Store
     ##
 
-    def _read_from_disk(self):
-        """ read data in from storage and store it and an index in attrs"""
+    def _initialize_data(self):
+        """ populate our index attrs """
 
         # read in data from our db, which is a one-to-one mapping of domains
         #  to websites (website == server:port)
@@ -269,6 +334,9 @@ DOMAIN NAME                   SERVER        PORT  ALIASES
         # should we do some integrity checking here? i.e., make sure that all
         # domains have a www counterpart? check for dupes?
 
+        ##
+        # self.domains
+        ##
         # filter out www's for our users, we will add them back in when we
         #  write to disk
         domains = {}
@@ -276,8 +344,9 @@ DOMAIN NAME                   SERVER        PORT  ALIASES
             if not domain.startswith('www.'):
                 domains[domain] = rawdata[domain]
 
-        # we also keep an index around
-        #  a one-to-many mapping of websites to domains
+        ##
+        # self.aliases
+        ##
         aliases = {}
         for domain in domains:
             website = domains[domain]
@@ -286,8 +355,25 @@ DOMAIN NAME                   SERVER        PORT  ALIASES
             else:
                 aliases[website] = [domain]
 
-        self.domains = domains.copy()
-        self.aliases = aliases.copy()
+        ##
+        # self.servers and self.websites
+        ##
+        servers, websites = self._available_websites()
+
+
+        ##
+        # self.codenames
+        ##
+        codenames = {}
+        for website in websites:
+            server_port = website.split('@')[1]
+            codenames[server_port] = website
+
+        self.aliases   = aliases.copy()
+        self.codenames = codenames.copy()
+        self.domains   = domains.copy()
+        self.servers   = servers.copy()
+        self.websites  = websites.copy()
 
 
     def _write_to_disk(self):
@@ -379,3 +465,98 @@ DOMAIN NAME                   SERVER        PORT  ALIASES
         if d1 < d2: return -1
         if d1 > d2: return 1
     _domain_cmp = staticmethod(_domain_cmp)
+
+
+    def _available_websites():
+        """ return a list of ids of available websites;
+        website ids are of the form codename@hostname:port """
+
+        # in our tests we stub this out rather than actually instantiating an
+        #  http server
+
+        # get our hostnames from /etc/hosts
+        etc_hosts = file('/etc/hosts')
+        hosts = etc_hosts.read()
+        etc_hosts.close()
+
+        hostnames = []
+        for line in hosts.split('\n'):
+            # we were using linesep to split but were getting bad data
+            # -- sorry, not enough info, holdover from Cheeze
+            line = line.replace('\r','').strip()
+            if line == '' or line.startswith('#'):
+                continue
+            elif not line.count(' ') > 0 and \
+                 not line.count('\t') > 0:
+                raise PorterError, "Couldn't parse this line in your " \
+                                 + "etc/hosts file: %s" % line
+            else:
+                # line is parseable
+                try:
+                    ip, domains = [foo.strip() for foo in line.split(None,1)]
+                except:
+                    raise PorterError, "Couldn't parse this line in your " \
+                                     + "etc/hosts file: %s" % line
+                hostnames.extend([d.strip() for d in domains.split(' ') \
+                                             if d != ''])
+
+        # now get available websites from each host
+        #  and populate an index of servers
+        servers = {}
+        websites = {}
+
+        sys.stdout.write("Polling %s hosts for websites ... " % len(hostnames))
+        i = 1; good_servers = 0
+
+        for hostname in hostnames[:1]:
+            # loop through candidate hostnames, fail silently if we can't
+            #  connect or if the host is serving something other than Fellow/0.2
+            #  on port 8000
+
+            sys.stdout.write(str(i) + ' ')
+            sys.stdout.flush()
+            if i == len(hostnames):
+                print "... done."
+            i += 1
+
+            # first ping to see if the host is there at all
+            ping = Popen3('ping -c1 -t1 -q %s' % hostname).wait()
+            if ping != 0:
+                servers[hostname] = (0,'no ping')
+                continue
+
+            # try to connect on port 8000
+            try:
+                http = httplib.HTTPConnection(hostname,8000)
+                http.connect()
+            except socket.error:
+                servers[hostname] = (0,'no 8000')
+                continue
+
+            # issue our request
+            http.request('GET','/')
+            response = http.getresponse()
+            http.close()
+
+            # process the response
+            if not response.getheader('Server').startswith('Fellow/0.2'):
+                servers[hostname] = (0,'no fellow')
+                continue
+            else:
+                # this hostname is a Fellow that we can understand!
+                good_servers += 1
+                new_sites = loads(b64decode(response.read()))
+                j = 0
+                for site in new_sites:
+                    if websites.get(site) is not None:
+                        #raise PorterError, "duplicate website id"
+                        print 'dupe'
+                    else:
+                        websites[site] = new_sites[site]
+                        j += 1
+                servers[hostname] = (j,'live')
+
+        print "We found %s websites on %s servers." % (len(websites), good_servers)
+
+        return servers, websites
+    _available_websites = staticmethod(_available_websites)
--- Porter/branches/whit537-fellow/tests/test_fellow.py
+++ Porter/branches/whit537-fellow/tests/test_fellow.py
@@ -17,7 +17,16 @@ class TestFellowIntegration(unittest.TestCase):
         os.mkdir('var')
         self.porter = Porter(stdout=self.out)
 
+        # we aren't actually going to test against a live http server
+        self.porter._available_hosts = self._test_hosts
+        self.porter._read_from_disk
+
+        # some sample data
+        self.porter.onecmd("add zetaweb.com bridei 8010")
+        self.porter.onecmd("add example.net bridei 8020")
+
         # ... go!
+
 
     def tearDown(self):
         self.cleanUp()
@@ -30,6 +39,39 @@ class TestFellowIntegration(unittest.TestCase):
                 for datafile in os.listdir(test_dir):
                     os.remove(join(test_dir, datafile))
                 os.rmdir(test_dir)
+
+    def _test_hosts():
+        return ['foo', 'bar', 'baz']
+    _test_hosts = staticmethod(_test_hosts)
+
+
+    def testSimpleLs(self):
+        # be sure we are adding data right
+        self.porter.onecmd("ls")
+        self.assertEqual(self.out.getvalue(), 'example.net  zetaweb.com\n')
+
+    def testSimpleTabCompletion(self):
+        # are tabs getting expanded by TestCase?
+        self.porter.onecmd("ls zet\t")
+        self.assertEqual(self.out.getvalue(), 'zetaweb.com\n')
+
+    def testNoTabCompletion(self):
+        # this also works since we do startswith in ls
+        self.porter.onecmd("ls zet")
+        self.assertEqual(self.out.getvalue(), 'zetaweb.com\n')
+
+#    def testAvailableHosts(self):
+#        self.assertEqual(self.porter._available_hosts(),
+#                         ['manually check this list against your own /etc/hosts'])
+
+    def testAvailableWebsitesTestingStrategy(self):
+        """ we aren't actually going to test against a live http server """
+        self.assertEqual(self.porter._available_hosts(), ['foo', 'bar', 'baz'])
+
+    def testAvailableWebsitesTestingStrategy(self):
+        self.porter._available_hosts = self._test_hosts
+        self.porter._read_from_disk()
+        self.assertEqual(self.porter.websites, ['foo', 'bar', 'baz'])
 
 
 def test_suite():
