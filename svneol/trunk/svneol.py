4c399b81240fcd6b6a06aca6f030f37902e94afc
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2005-03-31 19:09:26 +0000

    yay!

--- svneol/trunk/svneol.py
+++ svneol/trunk/svneol.py
@@ -3,154 +3,54 @@
 # This program is beerware. If you like it, buy me a beer someday.
 # No warranty is expressed or implied.
 
-import os, sys
+import os, re, sys
+from glob import glob
+from sets import Set
+from ConfigParser import RawConfigParser
 
-ERROR = 'see man 1 svneol for usage'
+arg = sys.argv[1:2]
+if arg: top = arg[0]
+else:   top = '.'
+top = os.path.realpath(top)
 
-# first get and validate some options
-args = sys.argv[1:]
-
-# decide if we should overwrite an existing directory
-if '--force' in args:
-    FORCE = True
-    args.remove('--force')
+# parse the config file into a list of patterns matching text files
+config = RawConfigParser()
+config.read(os.path.expanduser('~/.subversion/config'))
+if not config.has_section('auto-props'):
+    print 'your subversion config file has no auto-props section'
 else:
-    FORCE = False
+    autoprops = config.options('auto-props')
+    globlist = []
+    for pattern in autoprops:
+        if 'svn:eol-style' in config.get('auto-props', pattern):
+            globlist.append(pattern)
+    globlist.sort()
 
-if len(args) not in [1,2]:
-    print ERROR
-else:
-    if len(args) == 1:
-        name = args[0]
-        port = '8080'
-    elif len(args) == 2:
-        name, port = args
+# walk the tree and get a list of paths to text files
+textfiles = Set()
+for path, dirs, files in os.walk(top):
+    for filename in files:
+        for pattern in globlist:
+            fullpattern = '%s/%s' % (path, pattern)
+            for textfile in glob(fullpattern):
+                textfiles.add(textfile)
+    if '.svn' in dirs: dirs.remove('.svn')
+textfiles = tuple(textfiles)
 
-    if not port.isdigit():
-        print 'second argument must be a port number'
-        print ERROR
-    else:
-        # set user, ports, & paths
-        current_user = os.popen4('whoami')[1].read()
+print 'found %s text files; cleaning...' % len(textfiles)
 
-        http_port = int(port)
-        ftp_port = http_port + 1
-        zeo_port = http_port + 9
+# now actually do the replacement
+win = re.compile(r'\r\n') # not worrying about mac for now
+i = 0
+for path in textfiles:
+    textfile = file(path, 'r+')
+    tmp = textfile.read()
+    dirty = len(win.findall(tmp))
+    if dirty > 0:
+        i += 1
+        print '  %s' % path
+        tmp = win.sub('\n', tmp)
+        textfile.seek(0); textfile.truncate(); textfile.write(tmp)
+    textfile.close()
 
-        root = os.path.realpath(name)
-        client_path = os.path.join(root, 'client-1')
-        server_path = os.path.join(root, 'server')
-
-        # make the base directory if it doesn't already exist
-        if os.path.exists(root) and not FORCE:
-            print '%s already exists; use --force to overwrite' % root
-        else:
-            os.system('rm -rf %s' % root)
-            os.makedirs(root)
-
-            # create and configure the ZEO server instance
-            os.system('/usr/local/zope/zope/bin/mkzeoinstance.py %s' % server_path)
-            confile = file(os.path.join(server_path,'etc','zeo.conf'), 'r+')
-            tmp = confile.read(); confile.seek(0); confile.truncate()
-            tmp = tmp.replace( '  address 9999'
-                             , '  address %s' % zeo_port)
-            confile.write(tmp)
-            confile.close()
-
-
-            # create and configure the ZEO client instance
-            os.system('/usr/local/zope/zope/bin/mkzopeinstance.py -d %s' % client_path)
-            confile = file(os.path.join(client_path,'etc','zope.conf'), 'r+')
-            tmp = confile.read(); confile.seek(0); confile.truncate()
-            tmp = tmp.replace( '#    effective-user chrism'
-                             , '    effective-user %s' % current_user)
-            tmp = tmp.replace( '  address 8080'
-                             , '  address %s' % http_port)
-            tmp = tmp.replace( '  address 8021'
-                             , '  address %s' % ftp_port)
-
-            tmp = tmp.replace("""\
-<zodb_db main>
-    # Main FileStorage database
-    <filestorage>
-      path $INSTANCE/var/Data.fs
-    </filestorage>
-    mount-point /
-</zodb_db>""", '')
-
-            tmp = tmp.replace("""\
-# <zodb_db main>
-#   mount-point /
-#   # ZODB cache, in number of objects
-#   cache-size 5000
-#   <zeoclient>
-#     server localhost:8100
-#     storage 1
-#     name zeostorage
-#     var $INSTANCE/var
-#     # ZEO client cache, in bytes
-#     cache-size 20MB
-#     # Uncomment to have a persistent disk cache
-#     #client zeo1
-#   </zeoclient>
-# </zodb_db>""","""\
-<zodb_db main>
-  mount-point /
-  # ZODB cache, in number of objects
-  cache-size 5000
-  <zeoclient>
-    server localhost:%s
-    storage 1
-    name zeostorage
-    var $INSTANCE/var
-    # ZEO client cache, in bytes
-    cache-size 20MB
-    # Uncomment to have a persistent disk cache
-    #client zeo1
-  </zeoclient>
-</zodb_db>""" % zeo_port)
-
-            confile.write(tmp)
-            confile.close()
-
-
-
-            # now install a script into rc.d
-            rcd = { 'client'   : os.path.join(client_path, 'bin', 'zopectl')
-                  , 'server'   : os.path.join(server_path, 'bin', 'zeoctl')
-                  , 'name'     : name
-                  , 'fullpath' : root
-                   }
-
-            rc_script = """\
-#!/bin/sh -
-#
-#	initialization/shutdown script for the zope instance at:
-#
-#     %(fullpath)s
-
-case "$1" in
-start)
-    %(server)s start && echo 'started %(name)s zeo server' && %(client)s start && echo 'started %(name)s zeo client'
-    ;;
-stop)
-    %(client)s stop && echo 'stopped %(name)s zeo client' && %(server)s stop && echo 'stopped %(name)s zeo server'
-    ;;
-restart)
-    echo 'restarting:'
-    %(client)s stop && echo 'stopped %(name)s zeo client' && %(server)s restart && echo 'restarted %(name)s zeo server' && %(client)s start && echo 'started %(name)s zeo client'
-    echo
-    ;;
-*)
-    echo "unknown option: $1 - should be 'start', 'stop', or 'restart'" >&2
-    ;;
-esac
-    	    """ % rcd
-
-            rc_path = '/usr/local/zope/rc.d/%(name)s.sh' % rcd
-            rc_file = file(rc_path, 'w+')
-            rc_file.write(rc_script)
-            rc_file.close()
-            os.system('chmod 0755 %s' % rc_path)
-
-            print 'zope+zeo instance successfully created'
+print 'done; cleaned up %s files' % i
