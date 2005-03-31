6b58af1d01dcc58ca0e5dd01289a47fee8cac018
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2005-03-31 01:08:31 +0000

    initial import, pattern of jrc

---
+++ zopeme/trunk/zopeme.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python
+# (c) 2005 Chad Whitacre <http://www.zetaweb.com/>
+# This program is beerware. If you like it, buy me a beer someday.
+# No warranty is expressed or implied.
+
+print 'hello world'
