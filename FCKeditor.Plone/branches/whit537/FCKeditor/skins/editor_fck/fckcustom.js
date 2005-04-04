384f73c4339282a1f7c3c8ad1be39d3a15f99905
Chad Whitacre <chad@zetaweb.com>
2005-04-04 20:21:31 +0000

    wiring in an fckcustom.js

---
+++ FCKeditor.Plone/branches/whit537/FCKeditor/skins/editor_fck/fckcustom.js
@@ -0,0 +1,3 @@
+// Override this file from your Product to provide a customized configuration
+// If the file is not present, the editor won't load, so we have this here
+// even when it is not used.
