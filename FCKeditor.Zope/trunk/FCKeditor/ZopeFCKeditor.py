076e7475439339a927b9a43619d15cc15682eb9f
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2005-04-22 13:25:42 +0000

    minimally working in Zope

--- FCKeditor.Zope/trunk/FCKeditor/ZopeFCKeditor.py
+++ FCKeditor.Zope/trunk/FCKeditor/ZopeFCKeditor.py
@@ -1,18 +1,23 @@
+# Python
+import types
+
 # Zope
 from Acquisition import Implicit
 from Persistence import Persistent
 from AccessControl.Role import RoleManager
 from OFS.SimpleItem import Item
+from OFS.PropertyManager import PropertyManager
 
 from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-
+import ExtensionClass
 
 # us
 from FCKeditor import FCKeditor
 
-class ZopeFCKeditor(FCKeditor, Implicit, Persistent, RoleManager, Item):
+class ZopeFCKeditor(FCKeditor, Implicit, Persistent, PropertyManager, \
+                    RoleManager, Item):
     """A Zope wrapper around FCKeditor
     """
 
@@ -22,17 +27,47 @@ class ZopeFCKeditor(FCKeditor, Implicit, Persistent, RoleManager, Item):
     title = ''
     meta_type = 'FCKeditor'
 
-    def __init__(self, id, *arg, **kw):
+    _properties = ( {'id':'InstanceName',   'type':'string', 'mode':'w'}
+                  , {'id':'Width',          'type':'string', 'mode':'w'}
+                  , {'id':'Height',         'type':'string', 'mode':'w'}
+                  , {'id':'ToolbarSet',     'type':'string', 'mode':'w'}
+                  , {'id':'Value',          'type':'string', 'mode':'w'}
+                  , {'id':'BasePath',       'type':'string', 'mode':'w'}
+                   )
 
+    manage_options = PropertyManager.manage_options +\
+                     ({'label':'View', 'action':''},) +\
+                     RoleManager.manage_options +\
+                     Item.manage_options
+
+    def __init__(self, id, *arg, **kw):
         self.id = id
 
         FCKeditor.__init__(self, *arg, **kw)
+        self._propertize_attrs()
+
+    def _propertize_attrs(self):
+        """In our base class we want to use instance attrs so that we can use
+        self.__dict__. However, self.__dict__ doesn't contain class attrs, and
+        Zope PropertyManager requires that object properties be class attrs. So
+        we need to convert certain of our inherited attrs into class attrs.
+        """
+        cls = self.__class__
+        attrs = [p['id'] for p in cls._properties]
+        for attr in attrs:
+            # don't need values here since they are overriden by instance attrs
+            cls.__dict__[attr] = None
 
     def Compatible(self):
         """only actually meaningful in Zope-space
         """
         return True
 
+    ##
+    # Management methods
+    ##
+
+    index_html = PageTemplateFile('www/manage_test.pt', globals())
 
 InitializeClass(ZopeFCKeditor)
 
