73c3fbdeb35ac1206f73414a65f5ae4136712732
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2005-05-02 22:46:39 +0000

    tagging first development release

---
+++ pytest/tags/0.3.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+classifiers = [
+    'Development Status :: 4 - Beta'
+  , 'Environment :: Console'
+  , 'Intended Audience :: Developers'
+  , 'License :: Freeware'
+  , 'Natural Language :: English'
+  , 'Operating System :: Unix'
+  , 'Programming Language :: Python'
+  , 'Topic :: Software Development :: Testing'
+                ]
+
+setup( name = 'pytest'
+     , version = '0.3'
+     , package_dir = {'':'site-packages'}
+     , packages = ['ASTutils','PyTest']
+     , description = 'Pytest is a testing interpreter for Python.'
+     , author = 'Chad Whitacre'
+     , author_email = 'chad [at] zetaweb [dot] com'
+     , url = 'http://www.zetadev.com/software/pytest/'
+     , classifiers = classifiers
+      )
