7d5684475c9dbfa66fe77e64f7e27692b254f977
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2005-05-27 15:37:23 +0000

    moderate overall refactor, need to update tests

--- FCKeditor.Zope/trunk/FCKeditor/__init__.py
+++ FCKeditor.Zope/trunk/FCKeditor/__init__.py
@@ -1,3 +1,14 @@
+# Imports
+# =======
+from AccessControl import allow_class
+from FCKexception import FCKexception  # the next import imports this from here
+from FCKeditor import FCKeditor as ZopeFCKeditor # so import order is important
+
+
+
+# Conditional Imports
+# ===================
+
 # Do we have CMFCore?
 try:
     from Products.CMFCore.DirectoryView import registerDirectory
@@ -8,25 +19,41 @@ except ImportError:
 # Do we have CMFPlone?
 try:
     from Products import CMFPlone
+    import PloneFCKmanager
     Plone = True
-except:
+except ImportError:
     Plone = False
 
 
-FCKglobals = globals()
 
-def initialize(context):
+# Anonymous Class
+# ===============
+# Not sure if this is goofy or cool, but we are wrapping FCKeditor for Zope in
+# here rather than in a full-blown class.
+
+# Zope doesn't like us messing with non-method attrs directly, so set up some
+# setters
+def SetConfig(self, key, val):
+    self.Config[key] = val
+setattr(ZopeFCKeditor, 'SetConfig', SetConfig)
+
+def SetProperty(self, key, val):
+    self.__dict__[key] = val
+setattr(ZopeFCKeditor, 'SetProperty', SetProperty)
+
+# and let us use the whole class unfettered from Scripts (Python)
+allow_class(ZopeFCKeditor)
+
+
+
+# Product Initialization
+# ======================
+
+FCKglobals = globals()
+def initialize(registrar):
 
     if CMFCore:
         registerDirectory('skins', FCKglobals)
 
-    import ZopeFCKeditor
-    ZopeFCKeditor.initialize(context)
-
     if Plone:
-        import PloneFCKmanager
-        PloneFCKmanager.initialize(context)
-
-
-class FCKexception(Exception):
-    """Error within the FCK connector application"""
+        PloneFCKmanager.initialize(registrar)
--- FCKeditor.Zope/trunk/FCKeditor/skins/fckeditor_plone/wysiwyg_fckeditor.py
+++ FCKeditor.Zope/trunk/FCKeditor/skins/fckeditor_plone/wysiwyg_fckeditor.py
@@ -1,40 +1,45 @@
 ##parameters=inputname, inputvalue
 import AccessControl
-from Products.PythonScripts.standard import structured_text as from_stx, \
-                                            newline_to_br   as from_plain
+from Products.PythonScripts.standard import structured_text as stx2html, \
+                                            newline_to_br   as plain2html
+from Products.FCKeditor import ZopeFCKeditor
+
 
 # instantiate an FCKeditor
-fckeditor = context.portal_fckmanager.spawn(inputname)
+fckeditor = ZopeFCKeditor(inputname)
 
 # tell FCKeditor about our custom runtime config file:
 fckeditor.SetConfig('CustomConfigurationsPath', '/fckcustom.js')
-
-# default height is 200px -- too short
-fckeditor.manage_changeProperties(Height=500)
 
 # decide whether the user's browser can support a WYSIWidget
 useragent = context.REQUEST['HTTP_USER_AGENT']
 fckeditor.SetCompatible(useragent)
 
+return fckeditor.Create()
+
+# default height is 200px -- too short
+fckeditor.SetProperty('Height', 500)
+
 # We convert from whatever format the text is stored in to HTML. The FCKeditor
 # object is responsible for HTML-encoding the value. And then the form action is
 # responsible to convert back from HTML to whatever.
 text_format = getattr(context, 'text_format', 'html')
-default_transform = lambda x: x
-transforms = { 'structured-text' : from_stx
-             , 'plain'           : from_plain
-             , 'html'            : default_transform
-              }
-transform = transforms.get(text_format, default_transform)
-HTML = transform(inputvalue)
+if text_format == 'html':
+    HTML = inputvalue
+if text_format == 'plain':
+    HTML = plain2html(inputvalue)
+elif text_format == 'structured-text':
+    HTML = stx2html(inputvalue)
+else:
+    HTML = inputvalue
 fckeditor.SetProperty('Value', HTML)
 
 # pick a toolbar set based on user role
 user = AccessControl.getSecurityManager().getUser()
 if user.has_role(['Manager',]):
-    fckeditor.SetProperty('ToolbarSet', 'Default')
+    fckeditor.ToolbarSet = 'Default'
 else:
-    fckeditor.SetProperty('ToolbarSet', 'Basic')
+    fckeditor.ToolbarSet = 'Basic'
 
 # return an HTML snippet
 return fckeditor.Create()
