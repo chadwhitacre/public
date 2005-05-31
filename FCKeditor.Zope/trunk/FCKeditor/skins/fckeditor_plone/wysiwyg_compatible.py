b17bb8e1532a8706a75204ac6813cd49d3f7dc91
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2005-05-31 12:48:56 +0000

    added license using boilerplater

--- FCKeditor.Zope/trunk/FCKeditor/skins/fckeditor_plone/wysiwyg_compatible.py
+++ FCKeditor.Zope/trunk/FCKeditor/skins/fckeditor_plone/wysiwyg_compatible.py
@@ -1,2 +1,22 @@
+#BOILERPLATE###################################################################
+#                                                                             #
+#  This package wraps FCKeditor for use in the Zope web application server.   #
+#  Copyright (C) 2005 Chad Whitacre <http://www.zetadev.com/>                 #
+#                                                                             #
+#  This library is free software; you can redistribute it and/or modify it    #
+#  under the terms of the GNU Lesser General Public License as published by   #
+#  the Free Software Foundation; either version 2.1 of the License, or (at    #
+#  your option) any later version.                                            #
+#                                                                             #
+#  This library is distributed in the hope that it will be useful, but        #
+#  WITHOUT ANY WARRANTY; without even the implied warranty of                 #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser    #
+#  General Public License for more details.                                   #
+#                                                                             #
+#  You should have received a copy of the GNU Lesser General Public License   #
+#  along with this library; if not, write to the Free Software Foundation,    #
+#  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA                #
+#                                                                             #
+###################################################################BOILERPLATE#
 from Products.FCKeditor import PloneFCKeditor
 return PloneFCKeditor.IsCompatible(context.REQUEST['HTTP_USER_AGENT'])
