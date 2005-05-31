55fc4ffe5d64dd0f6e2b1fd279084249218db06b
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2005-05-31 19:54:09 +0000

    pruning

--- testosterone/trunk/testosterone/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup( name = 'testosterone'
-     , version = '0.1'
-     , py_modules = ['testosterone']
-     , description = 'A manly testing framework'
-     , author = 'Chad Whitacre'
-     , author_email = 'chad@zetaweb.com'
-     , url = 'http://www.zetaewb.com/'
-      )
