c6f5280acf354d215dc44788ada196031492c65c
Chad Whitacre <chad@zetaweb.com>
2005-05-31 19:53:16 +0000

    pruning

--- FCKeditor.Plone/tags/2.0-FC-Preview/FCKeditor/__init__.py
+++
@@ -1,8 +0,0 @@
-
-global fckeditor_globals
-fckeditor_globals=globals()
-
-from Products.CMFCore.DirectoryView import registerDirectory
-
-def initialize(context):
-    registerDirectory('skins', fckeditor_globals)
