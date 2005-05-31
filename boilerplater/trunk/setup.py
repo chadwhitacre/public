a5be7a38f2818f8922dd5d0797c1087dd0a7fccf
Chad Whitacre <chad@zetaweb.com>
2005-05-31 01:46:19 +0000

    reorg underway!

---
+++ boilerplater/trunk/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+classifiers = [
+    'Development Status :: 4 - Beta'
+  , 'Environment :: Console'
+  , 'Intended Audience :: Developers'
+  , 'License :: Freely Distributable'
+  , 'Natural Language :: English'
+  , 'Operating System :: Unix'
+  , 'Programming Language :: Python'
+  , 'Topic :: Software Development :: Build Tools'
+                ]
+
+setup( name = 'boilerplater'
+     , version = '0.1'
+     , package_dir = {'':'site-packages'}
+     , packages = ['Boilerplater']
+     , description = 'Boilerplater adds boilerplate to a tree of files.'
+     , author = 'Chad Whitacre'
+     , author_email = 'chad [at] zetaweb [dot] com'
+     , url = 'http://www.zetadev.com/software/boilerplater/'
+     , classifiers = classifiers
+      )
