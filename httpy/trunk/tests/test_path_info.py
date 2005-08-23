31c9ec7ee17e29dbdb29653cd606415142417a5f
Chad Whitacre <chad@zetaweb.com>
2005-08-23 23:16:44 +0000

    finished first test script

--- httpy/trunk/tests/test_path_info.py
+++ httpy/trunk/tests/test_path_info.py
@@ -3,7 +3,10 @@ import sys
 sys.path.insert(0, os.path.realpath('..'))
 
 import httpy
+import stat
 import unittest
+from medusa import http_server
+from httpyTestCase import httpyTestCase
 
 default_server_config, default_handler_config = httpy.parse_config('')
 default_request = ( None
@@ -13,15 +16,22 @@ default_request = ( None
                   , '1.1'
                   , ['Host: josemaria:8080', 'User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.10) Gecko/20050716 Firefox/1.0.6', 'Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5', 'Accept-Language: en-us,en;q=0.7,ar;q=0.3', 'Accept-Encoding: gzip,deflate', 'Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7', 'Keep-Alive: 300', 'Connection: keep-alive']
                    )
-from medusa import http_server
 
-class TestPathInfo(unittest.TestCase):
+def buildTestSite():
+    os.mkdir('root')
+    file('root/index.html','w')
+    os.mkdir('root/about')
+    os.mkdir('root/My Documents')
+    file('root/My Documents/index.html','w')
+
+
+class TestPathInfo(httpyTestCase):
 
     def setUp(self):
 
-        # rebuild a temporary website tree in ./root
-        os.system('rm -rf root')
-        os.system('tar jxf data/path_info.tbz')
+        # [re]build a temporary website tree in ./root
+        self.removeTestSite()
+        buildTestSite()
 
         # handler
         self.request = http_server.http_request(*default_request)
@@ -31,26 +41,75 @@ class TestPathInfo(unittest.TestCase):
         self.assertEqual(self.handler.root, os.path.realpath('./root'))
 
     def testBasic(self):
-        self.request.uri = '/about'
+        self.request.uri = '/index.html'
         self.handler.path_info(self.request)
-        expected = (os.path.realpath('root/about/index.html'), '/about')
+        expected = (os.path.realpath('root/index.html'), '/index.html')
+        actual = (self.request.path, self.request.uri)
+        self.assertEqual(expected, actual)
+
+    def testDefaultDocument(self):
+        self.request.uri = '/'
+        self.handler.path_info(self.request)
+        expected = (os.path.realpath('root/index.html'), '/')
         actual = (self.request.path, self.request.uri)
         self.assertEqual(expected, actual)
 
     def testEncodedURIGetsUnencoded(self):
         self.request.uri = '/My%20Documents'
         self.handler.path_info(self.request)
-        expected = (os.path.realpath('root/My Documents/index.pt'), '/My%20Documents')
+        expected = (os.path.realpath( 'root/My Documents/index.html')
+                                   , '/My%20Documents'
+                                     )
         actual = (self.request.path, self.request.uri)
         self.assertEqual(expected, actual)
 
-    def testDoubleRootRaisesError(self):
-        self.request.uri = '//about'
-        self.assertRaises(httpy.RequestError, self.handler.path_info, self.request)
+    def testDoubleRootRaisesBadRequest(self):
+        self.request.uri = '//index.html'
+        self.assertRaises( httpy.RequestError
+                         , self.handler.path_info
+                         , self.request
+                          )
+        try:
+            self.handler.path_info(self.request)
+        except httpy.RequestError, err:
+            self.assertEqual(err.code, 400)
+
+    def testNoDefaultRaisesForbidden(self):
+        self.request.uri = '/about'
+        self.assertRaises( httpy.RequestError
+                         , self.handler.path_info
+                         , self.request
+                          )
+        try:
+            self.handler.path_info(self.request)
+        except httpy.RequestError, err:
+            self.assertEqual(err.code, 403)
+
+    def testNotThereRaisesNotFound(self):
+        self.request.uri = '/not-there'
+        self.assertRaises( httpy.RequestError
+                         , self.handler.path_info
+                         , self.request
+                          )
+        try:
+            self.handler.path_info(self.request)
+        except httpy.RequestError, err:
+            self.assertEqual(err.code, 404)
+
+    def testOutsideRootRaisesBadRequest(self):
+        self.request.uri = '/../../../../../../../root'
+        self.assertRaises( httpy.RequestError
+                         , self.handler.path_info
+                         , self.request
+                          )
+        try:
+            self.handler.path_info(self.request)
+        except httpy.RequestError, err:
+            self.assertEqual(err.code, 400)
 
     def tearDown(self):
-        os.system('rm -rf root')
-        pass
+        self.removeTestSite()
+
 
 
 if __name__ == '__main__':
