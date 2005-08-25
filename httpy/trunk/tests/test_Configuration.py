9c79f47a5550eea79da49ac836b74e931818582c
Chad Whitacre <chad@zetaweb.com>
2005-08-25 17:55:59 +0000

    finished first testing pass through Config

--- httpy/trunk/tests/test_Configuration.py
+++ httpy/trunk/tests/test_Configuration.py
@@ -7,10 +7,10 @@ from ConfigurationTestCase import ConfigurationTestCase
 
 
 class TestConfigurationDefaults(ConfigurationTestCase):
-    """Put it all together. This test __init__
+    """Put it all together. These test __init__.
     """
 
-    def test(self):
+    def testDefaults(self):
 
         # expected -- these are the defaults
         server = {}
@@ -20,7 +20,7 @@ class TestConfigurationDefaults(ConfigurationTestCase):
         handler['root'] = os.path.realpath('.')
         handler['defaults'] = ('index.html', 'index.pt')
         handler['extensions'] = ('pt',)
-        handler['mode'] = 'development'
+        handler['mode'] = 'deployment'
         server = self.dict2tuple(server)
         handler = self.dict2tuple(handler)
 
@@ -28,6 +28,53 @@ class TestConfigurationDefaults(ConfigurationTestCase):
 
         self.assertEqual(server, self.dict2tuple(actual.server))
         self.assertEqual(handler, self.dict2tuple(actual.handler))
+
+
+    def testOverlapProperly(self):
+
+        # set up website
+        os.mkdir('root')
+
+        # set up environment
+        os.environ['HTTPY_MODE'] = 'development' # should be retained
+        os.environ['HTTPY_PORT'] = '9000'        # should be overriden
+
+        # set up configuration file
+        conf = file('httpy.conf', 'w')
+        conf.write(os.linesep.join([
+            "[server]"
+          , "port: 537"                 # should be retained
+          , ""
+          , "[handler]"
+          , "root = /etc"               # should be overridden
+          , "defaults = default.asp"    # should be retained
+          , "extensions= asp"           # should be retained
+           ]))
+        conf.close()
+
+        argv = [ '-r','root'
+               , '-f','httpy.conf'
+                ]
+
+        # expected
+        server = {}
+        server['ip'] = ''                               # default
+        server['port'] = 537                            # file
+        handler = {}
+        handler['root'] = os.path.realpath('./root')    # opts
+        handler['defaults'] = ('default.asp',)          # file
+        handler['extensions'] = ('asp',)                # file
+        handler['mode'] = 'development'                 # env
+        server = self.dict2tuple(server)
+        handler = self.dict2tuple(handler)
+
+        self.config.__init__(argv)
+        actual = self.config
+
+        self.assertEqual(server, self.dict2tuple(actual.server))
+        self.assertEqual(handler, self.dict2tuple(actual.handler))
+
+        os.rmdir('root')
 
 
 def test_suite():
