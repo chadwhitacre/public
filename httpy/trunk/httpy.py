e61844e01c901568b1969488cad9127945a1da01
Chad Whitacre <chad@zetaweb.com>
2005-08-30 11:44:00 +0000

    and we're off

--- httpy/trunk/httpy.py
+++ httpy/trunk/httpy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""httpy is an uncomplicated Python webserver. `man 1 httpy' for details.
+"""httpy -- a straightforward Python webserver. `man 1 httpy' for details.
 
 The main() function below is inspired by Guido:
 
@@ -10,28 +10,26 @@ The main() function below is inspired by Guido:
 __version__ = (0,1)
 __author__ = 'Chad Whitacre <chad@zetaweb.com>'
 
-import asyncore
 import sys
 
-from medusa import http_server
+from httpy.Server import Server
 
-from httpy.Handler import Handler
-from httpy.Configuration import ConfigError
-from httpy.Configuration import Configuration
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
     try:
-        config = Configuration(argv)
+        #config = Configuration(argv)
+        config = {}
     except ConfigError, err:
         print >> sys.stderr, err.msg
         print >> sys.stderr, "`man 1 httpy' for usage."
         return 2
     else:
-        server = http_server.http_server(**config.server)
-        server.install_handler(Handler(**config.handler))
-        asyncore.loop()
+        addr = ('', 8080)
+        server = Server(addr)
+        server.serve_forever()
+
 
 if __name__ == "__main__":
     sys.exit(main())
