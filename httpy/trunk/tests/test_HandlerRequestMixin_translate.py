953b10a0c1f4f95db02e268a4e1bd6b54c6b7c0e
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2005-09-06 03:30:19 +0000

    YES!!!!!!!!!!!!!

--- httpy/trunk/tests/test_HandlerRequestMixin_translate.py
+++ httpy/trunk/tests/test_HandlerRequestMixin_translate.py
@@ -4,7 +4,18 @@ import os
 import unittest
 
 from httpy.HandlerRequestMixin import HandlerRequestMixin
+from httpy.Response import Response
 from httpyTestCase import httpyTestCase
+
+
+# later
+DUMMY_APP = """\
+class Transaction:
+    def __init__(self, config):
+        return config
+    def process(self, request):
+        raise "heck" """
+#file('root/app1/app.py', 'w').write(DUMMY_APP)
 
 
 class TestTranslate(httpyTestCase):
@@ -21,6 +32,9 @@ class TestTranslate(httpyTestCase):
         os.mkdir('root')
         file('root/index.html', 'w').write('hello world')
         os.mkdir('root/app1')
+        os.mkdir('root/app2')
+        os.mkdir('root/app2/__')
+
 
     def testBasic(self):
         root = self.siteroot
@@ -31,23 +45,84 @@ class TestTranslate(httpyTestCase):
         actual = self.mixin.translate(root, apps, path)
         self.assertEqual(expected, actual)
 
-    def testAppNotThere(self):
+    def testAppNotThereRaises404(self):
         root = self.siteroot
-        apps = []
+        apps = ['/not-there']
+        path = '/not-there'
+
+        try:
+            self.mixin.translate(root, apps, path)
+        except Response, response:
+            self.assertEqual(response.code, 404)
+
+    def testNonWebsiteRootApp(self):
+        root = self.siteroot
+        apps = ['/app1']
+        path = '/app1'
+
+        expected = (os.path.join(self.siteroot, 'app1'), None)
+        actual = self.mixin.translate(root, apps, path)
+        self.assertEqual(expected, actual)
+
+    def testOtherAppsAvailableButNotMatchedReturnsRoot(self):
+        root = self.siteroot
+        apps = ['/app1']
         path = '/not-there'
 
         expected = (self.siteroot, None)
         actual = self.mixin.translate(root, apps, path)
         self.assertEqual(expected, actual)
 
-    def testOtherApp(self):
+    def testEtcPasswdRaises400(self):
         root = self.siteroot
         apps = []
-        path = '/app1'
+        path = '/../../../../../../../../../../etc/master.passwd'
 
-        expected = (self.siteroot, None)
+        try:
+            self.mixin.translate(root, apps, path)
+        except Response, response:
+            self.assertEqual(response.code, 403)
+
+    def testSubdirStillMatches(self):
+        root = self.siteroot
+        apps = ['/app1']
+        path = '/app1/index.html'
+
+        expected = (os.path.join(self.siteroot, 'app1'), None)
         actual = self.mixin.translate(root, apps, path)
         self.assertEqual(expected, actual)
+
+    def testAppHasMagicDirectory(self):
+        root = self.siteroot
+        apps = ['/app2']
+        path = '/app2'
+
+        expected = ( os.path.join(self.siteroot, 'app2')
+                   , os.path.join(self.siteroot, 'app2', '__')
+                    )
+        actual = self.mixin.translate(root, apps, path)
+        self.assertEqual(expected, actual)
+
+    def testButDirectlyAccessingItRaises404(self):
+        root = self.siteroot
+        apps = ['/app2']
+        path = '/app2/__'
+
+        try:
+            self.mixin.translate(root, apps, path)
+        except Response, response:
+            self.assertEqual(response.code, 404)
+
+    def testSubdirsToo(self):
+        root = self.siteroot
+        apps = ['/app2']
+        path = '/app2/__/app.py' # Doesn't actually exist, proving we are
+                                 # catching it here.
+        try:
+            self.mixin.translate(root, apps, path)
+        except Response, response:
+            self.assertEqual(response.code, 404)
+
 
 
 def test_suite():
