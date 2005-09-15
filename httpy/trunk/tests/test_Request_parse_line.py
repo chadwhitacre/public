d734a8bd1693057610c129efa793ffc89db16fa1
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2005-09-15 03:36:53 +0000

    finally working again

--- httpy/trunk/tests/test_Request_parse_line.py
+++ httpy/trunk/tests/test_Request_parse_line.py
@@ -10,31 +10,48 @@ from httpy.Request import Request
 from RequestTestCase import PARTS
 
 
+
+DUMMY_URI = [ ('fragment','fragment')
+            , ('netloc','netloc')
+            , ('parameters','parameters')
+            , ('path','/path')
+            , ('query','query')
+            , ('scheme','http')
+             ]
+
 class RequestParsesLineTests:
+
     def testExactLine(self):
         self.request.received(self.LINE)
-        expected = self.LINE.strip(),
-        actual = self.request.raw_line
-        for key,value in things.items():
-            self.assertEqual(value,)
+        things = { 'method': 'POST'
+                 , 'path': '/path'
+                 , 'uri': DUMMY_URI
+                  }
+        for key, value in things.items():
+            expected = value
+            actual = getattr(self.request,key)
+            if key == 'uri':
+                actual = sorted([i for i in actual.items()])
+            self.assertEqual(expected, actual)
+
 
 class TestRequestParsesLineCRLF(RequestParsesLineTests,unittest.TestCase):
     def setUp(self):
         self.request = Request(default_adj)
         newline='\r\n'
-        (self.IE_CRAP,self.LINE,self.HEADERS,self.BODY,self.POST)=PARTS(newline)
+        (self.IE_CRAP,self.LINE,self.LINE2,self.HEADERS,self.HEADERS2,self.BODY,self.POST,self.GET)=PARTS(newline)
 
 class TestRequestParsesLineCR(RequestParsesLineTests,unittest.TestCase):
     def setUp(self):
         self.request = Request(default_adj)
         newline='\r'
-        (self.IE_CRAP,self.LINE,self.HEADERS,self.BODY,self.POST)=PARTS(newline)
+        (self.IE_CRAP,self.LINE,self.LINE2,self.HEADERS,self.HEADERS2,self.BODY,self.POST,self.GET)=PARTS(newline)
 
 class TestRequestParsesLineLF(RequestParsesLineTests,unittest.TestCase):
     def setUp(self):
         self.request = Request(default_adj)
         newline='\n'
-        (self.IE_CRAP,self.LINE,self.HEADERS,self.BODY,self.POST)=PARTS(newline)
+        (self.IE_CRAP,self.LINE,self.LINE2,self.HEADERS,self.HEADERS2,self.BODY,self.POST,self.GET)=PARTS(newline)
 
 def test_suite():
     from unittest import TestSuite, makeSuite
