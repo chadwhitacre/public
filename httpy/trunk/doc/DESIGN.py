ffc1a568e55fbb8b6410bed3f7a6a461bd0642a2
Chad Whitacre <chad@zetaweb.com>
2005-09-22 14:45:14 +0000

    added AppCache, doesn't seem to be helping :-/

--- httpy/trunk/doc/DESIGN.py
+++ httpy/trunk/doc/DESIGN.py
@@ -1,87 +1,27 @@
-Here is the entire request handling process, represented in pseudocode
+httpy is an HTTP bridge for your Python applications. There are really only
+three things going on:
 
+    1. Request -- When an HTTP request comes in off the wire, we give it a
+    rather literal object representation and hand that off to your application.
 
-# Request Comprehension
-# =====================
+    2. Application -- Because HTTP is a stateless protocol, httpy expects you to
+    represent each transaction within your application as an autonomous,
+    standalone unit, encapsulated in a Transaction object.
 
-    There are two or three parts to every Request: Request-line, headers, body
-
-    read the Request-Line into method, uri, abs_path, querystring, and version
-    read the headers into headers
-    read any message body into body
-      if Content-Length and not Transfer-Encoding:
-          stream.read(Content-Length)
-      elif Transfer-Encoding:
-          http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.4.6
-      else:
-          raise
-
-    class Request:
-        """An HTTP Request message.
-
-            http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html
-
-        """
-
-        def __init__(self, stream):
-            """Takes an incoming HTTP stream.
-            """
-            self.parse_line(stream)
-            self.parse_headers(stream)
-            self.parse_body(stream)
-
-        def parse_line(self, stream):
-            pass
-
-        def parse_headers(self, stream):
-            pass
-
-        def parse_body(self, stream):
-            pass
+    3. Response -- When your application has finished processing a transaction,
+    it raises a Response object. This carries a payload back up into the httpy
+    machinery, which writes back onto the wire.
 
 
 
-# State
-# =====
-
-    class Transaction(dict):
-        """A single transaction within an application.
-        """
-
-        def __init__(self, request):
-            """Takes an httpy.Request object.
-            """
-            self.request = request
-            self.query = cgi.parse_qs(request.querystring)
-            self.cookie = Cookie.SimpleCookie(request.headers.get('Cookie','')
-            self.post = self.parse_post(request.body)
-            self.__dict__ = self.harmonize()
-
-        def process(self):
-            self.response = Response()
-
-        def end(self, response):
-            """Given an httpy.Response, initiate the end of the transaction.
-            """
-            raise StatusCodes.OK(response)
+filesystem website root
+url-space app directories
+filesystem app directories
+filesystem magic directories
+app objects (module/package) (could an app be a class in __init__.py?)
+transaction objects (class)
 
 
-
-# Response
-# ========
-
-    class Response:
-        """An HTTP Response message.
-
-            http://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html
-
-        """
-
-        def __init__(self, state):
-            """Takes an httpy.Transaction.
-            """
-            self.state = state
-            self.request = state.request
 
 
 
@@ -114,7 +54,7 @@ The Stack
         4. applications
             specific apps:
                 conversation (forums, discussion mailing lists)
-                distribution lists (announcement lists)
+                mailing lists
                 content streams (blog, news)
                 CMS (end-user-managed publications)
                 catalog (i.e., gallery/album)
@@ -151,17 +91,7 @@ The Stack
 
     BOTTOM
 
-
-httpy uses stdlib tools for #1
-httpy implements #2 directly
-    shooting for unconditional compliance
-    checking out Co-Advisor
-httpy provides stub request/response implementations
-httpy provides a basic publication application
-    simply serves static content from the filesystem
-
-
-http errata:
+HTTP errata: (but HTML version is auto-generated)
     ust not
     self- elimiting
     ransfer-length
@@ -169,39 +99,3 @@ http errata:
     varriant
     is not be construed
     section 5.1.1 of RFC 2046
-
-
-Ok, now we are talking about several components:
-
-    httpy -- uncomplicated Python webserver
-        unconditionally compliant with HTTP/1.1 (Co-Advisor?)
-        gives you hooks
-            __/hook.py
-            __/site-packages -- prepended to sys.path if present
-
-
-    httpy
-    httpy.Configurator
-    httpy.Request
-    httpy.Response
-    httpy.Server
-    httpy.StatusCodes
-
-    flynn -- toolkit on top of httpy
-
-        flynn.site
-            __/frame.pt
-            __/error.pt
-            __/context.py
-            __/apps
-            foo-app/__init__.py
-        flynn.http
-            Request
-            Response
-            StatusCodes
-        flynn.tron
-            security program; ideal: kerberos ticket auth
-        flynn.utils
-            Configurator?
-                harmonizes input from defaults, env, file, opts
-            Navigator
