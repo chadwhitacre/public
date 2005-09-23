256c6792e3ef5f9597bb57930e0ae0691d9230a2
Chad Whitacre <chad@zetaweb.com>
2005-09-23 02:56:11 +0000

    four modules tests, two to go

---
+++ httpy/trunk/tests/test_AppCache.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+import imp
+import os
+import unittest
+
+from httpy.AppCache import AppCache
+from httpy.NicolasLehuen import Entry
+from httpy import DefaultApp
+
+
+APP_BASE = """\
+from httpy.Response import Response
+
+class Transaction:
+
+    def __init__(self, config):
+        pass
+
+    def process(self, request):
+        response = Response(200)
+        response.headers['Content-Type'] = 'text/plain'
+        response.body = "Greetings, program!"
+        raise response
+
+"""
+
+APP0 = APP_BASE + "appnum = 0"
+APP1 = APP_BASE + "appnum = 1"
+APP2 = APP_BASE + "appnum = 2"
+
+
+class TestAppCache(unittest.TestCase):
+
+    def setUp(self):
+        self.apps = AppCache('development')
+
+        # [re]build a site in ./root
+        os.system('rm -rf root')
+        os.mkdir('root')
+        os.mkdir('root/__')
+        file('root/__/app.py','w').write(APP0)
+        os.mkdir('root/app1')
+        os.mkdir('root/app1/__')
+        file('root/app1/__/app.py','w').write(APP1)
+        os.mkdir('root/app2')
+        os.mkdir('root/app2/__')
+        file('root/app2/__/app.py','w').write(APP2)
+
+        os.environ['HTTPY_VERBOSITY'] = '0'
+
+
+    # demonstration
+    # =============
+
+    def testAttrAccess(self):
+        # inner dict is initially empty
+        self.assertEqual(self.apps._dict.keys(), [])
+
+        # simple access will return the object and prime the cache
+        app = self.apps['root/__']
+        self.assertEqual(app.appnum, 0)
+        self.assertEqual(self.apps._dict.keys(), ['root/__'])
+
+
+    # get_app
+    # =======
+
+    def testGetApp(self):
+        fp, pathname, description = imp.find_module('app', ['root/__'])
+        expected = imp.load_module('app', fp, pathname, description)
+        import sys
+        del(sys.modules['app'])
+        os.remove('root/__/app.pyc')
+
+        actual = self.apps.get_app('root/__')
+        self.assertEqual(expected.appnum, actual.appnum)
+
+    def testMagicDirectoryIsNone(self):
+        expected = DefaultApp
+        actual = self.apps.get_app(None)
+        self.assertEqual(expected, actual)
+
+    def testTwoModulesBothGetImported(self):
+        app1 = self.apps.get_app('root/app1/__')
+        self.assertEqual(app1.appnum, 1)
+        app2 = self.apps.get_app('root/app2/__')
+        self.assertEqual(app2.appnum, 2)
+
+    def testImportErrorRaisesButGetsCleanedUp(self):
+        self.assertRaises(ImportError, self.apps.get_app, '/app3/__')
+        import sys
+        self.assert_('app3/__' not in sys.path)
+        self.assert_('app' not in sys.modules)
+
+
+
+    # check
+    # =====
+
+    def testCheckDepModeUnloadedGetsLoaded(self):
+        entry = Entry('root/__')
+        self.apps.dev_mode = False
+        self.assertEqual(self.apps._dict, {})
+
+        expected = self.apps.get_app('root/__')
+        os.remove('root/__/app.pyc')
+        actual = self.apps.check('root/__', entry)
+        self.assertEqual(expected.appnum, actual.appnum)
+
+    def testCheckDepModeLoadedNotReloaded(self):
+        entry = Entry('root/__')
+        entry.loaded = True
+        self.apps.dev_mode = False
+
+        expected = None
+        actual = self.apps.check('root/__', entry)
+        self.assertEqual(expected, actual)
+
+    def testCheckDevModeLoadedGetsReloaded(self):
+        entry = Entry('root/__')
+        entry.loaded = True
+        self.apps.dev_mode = True
+
+        expected = self.apps.get_app('root/__')
+        os.remove('root/__/app.pyc')
+        actual = self.apps.check('root/__', entry)
+        self.assertEqual(expected.appnum, actual.appnum)
+
+
+    def tearDown(self):
+        os.system('rm -rf root')
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestAppCache))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main()
