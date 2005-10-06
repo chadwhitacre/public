cda80a4f910337b28bcc006ae5fa2fdeeac5ffb9
Chad Whitacre <chad@zetaweb.com>
2005-10-06 13:18:22 +0000

    baked MSGS

--- citlib/trunk/tests/test_CitConn_messages.py
+++ citlib/trunk/tests/test_CitConn_messages.py
@@ -12,17 +12,28 @@ class Tests(unittest.TestCase):
 
     def setUp(self):
         self.cit = CitConn()
-        self.cit._listen = True
+        self.cit._listen = 0
         self.cit.USER('test')
         self.cit.PASS('testing')
 
         # bootstrapping a bit here
-        for message in self.cit.MSGS():
-            self.cit.DELE(message)
+        self.rmmsgs()
 
     def tearDown(self):
         self.cit.LOUT()
         self.cit.QUIT()
+
+    def addmsg(self):
+        self.cit.ENT0(1,'',0,1,'cheese','',1,'','',message = 'BLAM!!!!!!!')
+
+    def addmsgs(self, r):
+        for i in range(r):
+            self.addmsg()
+        self.assert_(len(self.cit.MSGS()) == r)
+
+    def rmmsgs(self):
+        for msgnum in self.cit.MSGS():
+            self.cit.DELE(msgnum)
 
 
 
@@ -35,6 +46,43 @@ class Tests(unittest.TestCase):
     def test_MSGS_badScope(self):
         self.assertRaises(ValueError, self.cit.MSGS, 'fun')
 
+    def test_MSGS_needNumNoNum(self):
+        self.assertRaises(ValueError, self.cit.MSGS, 'last')
+
+    def test_MSGS_needNumBadNum(self):
+        self.assertRaises(ValueError, self.cit.MSGS, 'last', 'fooey')
+
+    def test_MSGS_needNumGoodNum(self):
+        self.addmsgs(5)
+        msgs = self.cit.MSGS('all')
+        self.assertEqual(len(msgs), 5)
+        msgs = self.cit.MSGS('last', 2)
+        self.assertEqual(len(msgs), 2)
+
+    def test_MSGS_paramsBasic(self):
+        self.addmsgs(4)
+        msgs = self.cit.MSGS('all', q={})
+        self.assertEqual(len(msgs), 4)
+        self.cit.ENT0(1,'',0,1,'teehee','',1,'','',message = 'YES!!!!!!!')
+        self.assertEqual(len(self.cit.MSGS()), 5)
+        msgs = self.cit.MSGS('all', q={'subj':'teehee'})
+        self.assertEqual(len(msgs), 1)
+        msgs = self.cit.MSGS('all', q={'subj':'cheese'})
+        self.assertEqual(len(msgs), 4)
+
+    def _test_MSGS_paramsMultiple(self):
+        # no obvious second parameter to use at this point
+        pass
+
+    def test_MSGS_headers_basic(self):
+        self.addmsgs(2)
+        expected = [ (445, 1128604557, 'test', 'josemaria', 'test@zetadev.com',
+                      'cheese', '')
+                   , (446, 1128604557, 'test', 'josemaria', 'test@zetadev.com',
+                      'cheese', '')
+                    ][1][5]
+        actual = self.cit.MSGS(headers=True)[1][5]
+        self.assertEqual(expected, actual)
 
 
 
@@ -61,29 +109,20 @@ class Tests(unittest.TestCase):
     # DELE
 
     def test_DELE(self):
-        r = 10
-        for i in range(r):
-            self.cit.ENT0(1,'',0,1,'cheese','',1,'','',message = 'BLAM!!!!!!!')
-        self.assert_(len(self.cit.MSGS()) == r)
+        self.addmsgs(3)
         for message in self.cit.MSGS():
             self.cit.DELE(message)
         self.assert_(len(self.cit.MSGS()) == 0)
 
     def test_DELE_takesInt(self):
-        r = 3
-        for i in range(r):
-            self.cit.ENT0(1,'',0,1,'cheese','',1,'','',message = 'BLAM!!!!!!!')
-        self.assert_(len(self.cit.MSGS()) == r)
+        self.addmsgs(1)
         for msgnum in self.cit.MSGS():
             self.assert_(isinstance(msgnum, int))
             self.cit.DELE(msgnum)
         self.assert_(len(self.cit.MSGS()) == 0)
 
     def test_DELE_takesLongInt(self):
-        r = 3
-        for i in range(r):
-            self.cit.ENT0(1,'',0,1,'cheese','',1,'','',message = 'BLAM!!!!!!!')
-        self.assert_(len(self.cit.MSGS()) == r)
+        self.addmsgs(1)
         for msgnum in self.cit.MSGS():
             msgnum = long(msgnum)
             self.assert_(isinstance(msgnum, long))
