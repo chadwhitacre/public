1c403d0ef41a7e31c0572571118f1a026e6b3a15
Chad Whitacre <chad@zetaweb.com>
2005-10-10 15:31:55 +0000

    successful ECHO

---
+++ zetaindex/trunk/bin/zetaindex.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+"""
+
+__version__ = (0, 1)
+__author__ = 'Chad Whitacre <chad@zetaweb.com>'
+
+from zeta.index.Server import Server
+
+
+def main(argv=None):
+
+    if argv is None:
+        argv = sys.argv[1:]
+
+    server = Server()
+    server.start()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+
+
