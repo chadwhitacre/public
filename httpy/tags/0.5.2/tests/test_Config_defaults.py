9388b180bfb8f601a8b564b4756cb5259496dbac
Chad Whitacre <chad@zetaweb.com>
2005-10-14 20:32:21 +0000

    tagging dev milestone: approaching 0.6 release!

---
+++ httpy/tags/0.5.2/tests/test_Config_defaults.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import unittest
+
+from TestCaseHttpy import TestCaseHttpy
+
+
+class TestConfigDefaults(TestCaseHttpy):
+
+    want_config = True
+
+    def testDefaults(self):
+
+        d = {}
+        d['ip'] = ''
+        d['port'] = 8080
+        d['mode'] = 'deployment'
+        d['root'] = os.path.realpath('.')
+        d['apps'] = ()
+        d['verbosity'] = 1
+
+        expected = self.dict2tuple(d)
+        actual = self.dict2tuple(self.config._defaults())
+
+        self.assertEqual(expected, actual)
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestConfigDefaults))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main()
