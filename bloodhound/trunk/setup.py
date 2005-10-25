1f153619d40eadaecc32274953c896c038524ffe
Chad Whitacre <chad@zetaweb.com>
2005-10-25 11:39:24 +0000

    yikes

--- bloodhound/trunk/setup.py
+++ bloodhound/trunk/setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup( name='bloodhound'
-     , version="0.9"
+setup( name='flynn'
+     , version="0.3"
+     , package_dir = {'':'site-packages'}
      , py_modules=['passwd']
       )
