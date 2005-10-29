cf2c58e1cde0e7f72d2d8c26c32adee20d02b1a1
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2005-10-29 16:12:06 +0000

    starting to drink the logging kool-aid

--- httpy/trunk/bin/httpy.py
+++ httpy/trunk/bin/httpy.py
@@ -7,9 +7,10 @@ The main() function below is inspired by Guido's:
 
 """
 
-__version__ = (0, 5)
+__version__ = (0, 7)
 __author__ = 'Chad Whitacre <chad@zetaweb.com>'
 
+import logging
 import os
 import signal
 import sys
@@ -20,8 +21,10 @@ from httpy.Config import Config
 from httpy.Server import Server
 from httpy.Server import RestartingServer
 
-
 def main(argv=None):
+
+    # Read in configuration options.
+    # ==============================
 
     if argv is None:
         argv = sys.argv[1:]
@@ -32,11 +35,23 @@ def main(argv=None):
         print >> sys.stderr, "`man 1 httpy' for usage."
         return 2
 
+
+    # Set up top-level logging.
+    # =========================
+
+    format = "%(name)-24s %(levelname)-8s %(message)s"
+    logging.basicConfig( level=logging.DEBUG
+                       , format=format
+                        )
+
+
+    # Instantiate and start a server.
+    # ===============================
+
     plain_jane = (  (os.environ['HTTPY_MODE'] == 'deployment')
                  or (sys.platform == 'win32')
                  or ('HTTPY_PLAIN_JANE' in os.environ)
                    )
-
     ServerClass = plain_jane and Server or RestartingServer
     server = ServerClass(config)
     server.start()
