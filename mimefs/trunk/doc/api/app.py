e630f1e496c4d5902f39fe03ed7d17c7a00411d4
Chad Whitacre <chad@zetaweb.com>
2006-01-19 22:04:10 +0000

    BLAM!!!!!!!

--- mimefs/trunk/doc/api/app.py
+++ mimefs/trunk/doc/api/app.py
@@ -14,18 +14,14 @@ from httpy.Response import Response
 # Set up to use reStructuredText if we have it.
 
 def _pre(s):
-    """This needs to be smarter
-    """
-    s = s.splitlines()
-    for i in range(len(s)):
-        if s[i].startswith('        '):
-            s[i] = s[i][8:]
-    return '<pre class="noshade">%s</pre>' % '\n'.join(s)
+    return '<pre class="noshade">%s</pre>' % s
 
 try:
     from docutils.core import publish_string
     BODY = re.compile('.*?<body>(.*?)</body>.*', re.DOTALL)
     def _reST(s):
+        if not isinstance(s, basestring):
+            return ''
         doc = publish_string(s, writer_name='html')
         return BODY.match(doc).group(1)
 except:
@@ -38,6 +34,9 @@ PARAMS = re.compile(' *def .*?\(self(?:, )?(.*?)\).*', re.DOTALL)
 class Application:
 
     def respond(self, request):
+
+        # Preen the request.
+        # ==================
 
         if request.method != 'GET':
             raise Response(501)
@@ -59,7 +58,7 @@ class Application:
         elif objpath.endswith('/'):
             objpath = objpath.rstrip('/')
         objnames = [n for n in objpath.split('/') if n]
-        acceptable = string.ascii_letters + string.digits
+        acceptable = string.ascii_letters + string.digits + '_'
         for name in objnames: # prevent snooping
             if name[0].isdigit():
                 raise Response(404)
@@ -75,24 +74,28 @@ class Application:
         objs = []
         for i in range(len(objnames)):
             curpath = '.'.join(objnames[:i+1])
-            obj = pydoc.locate(curpath)
+            obj = pydoc.locate(curpath, forceload=True)
             if obj is None:
                 raise Response(404)
             objs.append((curpath, obj))
-        obj = pydoc.locate(objpath)
         if want_routine and not inspect.isroutine(obj):
             raise Response(404)
         elif (not want_routine) and not request.path.endswith('/'):
             raise Response(302, '', {'Location':request.fullpath+'/'})
 
 
+        # Build a data structure.
+        # =======================
+        # This will be passed as a string replacement dictionary to TEMPLATE.
+        # By the time we get here, we have objpath, obj, objnames and objs.
+
         data = {}
 
         data['title'] = objpath
-        data['crumbs'] = self.getcrumbs(objs)
-        data['nav'] = '' # self.getnav(obj)
-        data['call'] = '' # self.getcall(obj)
-        data['content'] = '' # self.getcontent(obj)
+        data['crumbs'] = self.getcrumbs(objs, obj)
+        data['nav'] = self.getnav(objpath, objs)
+        data['callable'] = self.getcallable(obj, objnames[-1])
+        data['content'] = self.getcontent(objs, obj)
 
         data['version'] = ''
         if hasattr(obj, '__version__'):
@@ -100,63 +103,258 @@ class Application:
         data['date'] = time.strftime('%B %d, %Y')
         data['base'] = self.uri_root
 
+
+        # Send it out.
+        # ============
+
         response = Response(200)
         response.headers = {'Content-Type':'text/html'}
         response.body = TEMPLATE % data
         raise response
 
 
-    def getcrumbs(self, objs):
+
+    def getcrumbs(self, objs, curobj):
+        """Return a list of links to all parent packages, modules, and classes.
+        """
+        have_class = inspect.isclass(curobj)
         tmpl = '<a href="%s/%s%s">%s</a>'
         crumbs = []
         for path, obj in objs:
+            if inspect.isroutine(obj):
+                break
+            if have_class and inspect.isclass(obj):
+                break
             ending = '/'
             if inspect.isroutine(obj):
                 ending = '.html'
             crumbs.append(tmpl % ( self.uri_root
                                  , path.replace('.', '/')
                                  , ending
-                                 , obj.__name__
+                                 , obj.__name__.split('.')[-1]
                                   ))
         return '.'.join(crumbs)
 
 
-    def getnav(self, obj):
-        return ''
-        """
-        tmpl = '    <li><a href="%s">%s</a></li>'
-        nav = [tmpl % ('/', 'top')]
-        for c in callables:
-            item = tmpl % ('%s/%s.html' % (self.uri_root, c), c)
-            nav.append(item)
-        nav = '\n'.join(nav)
-
-        # Definition
-        source = inspect.getsource(obj)
-        params = PARAMS.match(source).group(1).replace('\n', '')
-        data['title'] = '%s(%s)' % (name, params)
+    def getcallable(self, obj, objname):
+        """Return an HTML snippet about the closest callable object.
         """
 
-        return nav
+        paratmpl = ' <span class="params">%s</span>'
+        objtype = ''
 
-    def getcall(self, obj):
+        if inspect.isclass(obj):
+            objtype = 'class'
+            if '__init__' in obj.__dict__:
+                # hasattr finds builtin.__init__, which confuses
+                # inspect.isfunction (called via getargspec)
+                func = getattr(obj, '__init__')
+            else:
+                func = False
+                params = paratmpl % '()'
+        elif inspect.isroutine(obj):
+            if inspect.ismethod(obj):
+                objtype = 'method'
+            else:
+                objtype = 'function'
+            func = obj
+        else:
+            return ''
+
+        if func:
+            args = inspect.getargspec(func)
+            params = inspect.formatargspec(*args)
+            params = params.replace('self, ', '').replace('self', '')
+            params = paratmpl % params
+
+        return '<h1>%s%s<span class="type">%s</span></h1>' % ( objname
+                                                             , params
+                                                             , objtype
+                                                              )
+
+
+    def getnav(self, objpath, objs):
+        """Return an HTML list of members for the closest module/package/class.
         """
-        """
-        return ''
 
-    def getcontent(self, objpath):
-        """Given an object, return a documentation body.
-        """
-        # find a module, look for a __docformat__
+        # Find the object to generate navigation from.
+        # ============================================
+
+        curobj = objs[-1][1]
+        navleader = curobj
+        if inspect.isroutine(curobj):
+            navleader = objs[-2][1]
+            objpath = '.'.join(objpath.split('.')[:-1])
 
 
+        # Get all public members.
+        # =======================
+        # An explicit __all__ is always obeyed, but we only do magic for
+        # classes. This is because module namespaces usually have tons of junk
+        # and only a little good stuff, whereas the opposite is usually true
+        # for class namespaces. Public members are sorted into packages/modules,
+        # methods/functions, and other.
+
+        magic = False
+        if hasattr(navleader, '__all__'):
+            all = navleader.__all__
+        else:
+            if inspect.isclass(navleader):
+                all = [n for n in navleader.__dict__ if not n.startswith('_')]
+                magic = True
+            else:
+                # Nothing to navigate.
+                return ''
+
+        members = [(n, getattr(navleader, n)) for n in all]
+
+        packages = []
+        modules = []
+        classes = []
+        routines = []
+        values = []
+
+        for m in members:
+            if inspect.isroutine(m[1]):
+                routines.append(m)
+            elif inspect.isclass(m[1]):
+                classes.append(m)
+            elif magic:
+                values.append(m)
+            elif self.ispackage(m[1]):
+                packages.append(m)
+            else:
+                modules.append(m)
+
+
+        # Build the HTML.
+        # ===============
+
+        html = []
+
+        curtmpl = '    <li class="current">%s</a></li>'
+        linktmpl = '    <li><a href="%s">%s</a></li>'
+        constmpl = '    <dt>%s</dt>\n<dd>%s</dd>'
+
+
+        if packages:
+            html.append('<h3>Packages</h3>')
+            html.append('<ul>')
+            for name, obj in packages:
+                if obj.__name__ == curobj.__name__:
+                    item = curtmpl % name
+                else:
+                    url = '%s/%s/%s/' % ( self.uri_root
+                                         , objpath.replace('.', '/')
+                                         , name
+                                          )
+                    item = linktmpl % (url, name)
+                html.append(item)
+            html.append('</ul>')
+
+
+        if modules:
+            html.append('<h3>Modules</h3>')
+            html.append('<ul>')
+            for name, obj in modules:
+                if obj.__name__ == curobj.__name__:
+                    item = curtmpl % name
+                else:
+                    url = '%s/%s/%s/' % ( self.uri_root
+                                         , objpath.replace('.', '/')
+                                         , name
+                                          )
+                    item = linktmpl % (url, name)
+                html.append(item)
+            html.append('</ul>')
+
+
+        if classes:
+            html.append('<h3>Classes</h3>')
+            html.append('<ul>')
+            for name, obj in classes:
+                if obj.__name__ == curobj.__name__:
+                    item = curtmpl % name
+                else:
+                    url = '%s/%s/%s/' % ( self.uri_root
+                                         , objpath.replace('.', '/')
+                                         , name
+                                          )
+                    item = linktmpl % (url, name)
+                html.append(item)
+            html.append('</ul>')
+
+
+        if routines:
+            heading = inspect.isclass(navleader) and 'Methods' or 'Functions'
+            html.append('<h3>%s</h3>' % heading)
+            html.append('<ul>')
+            for name, obj in routines:
+                if obj.__name__ == curobj.__name__:
+                    item = curtmpl % name
+                else:
+                    url = '%s/%s/%s.html' % ( self.uri_root
+                                         , objpath.replace('.', '/')
+                                         , name
+                                          )
+                    item = linktmpl % (url, name)
+                html.append(item)
+            html.append('</ul>')
+
+
+        if values:
+            html.append('<h3>Values</h3>')
+            html.append('<dl>')
+            for name, val in values:
+                html.append(constmpl % (name, val))
+            html.append('</dl>')
+
+
+        return '\n'.join(html)
+
+
+    def ispackage(self, obj):
+        if not hasattr(obj, '__file__'):
+            return False
+        return '__init__' in obj.__file__
+
+
+    def getcontent(self, objs, curobj):
+
+        if not curobj.__doc__:
+            return ''
+
+
+        # Trim initial spaces from docstring lines.
+        # =========================================
+
+        lines = curobj.__doc__.splitlines()
+        numspaces = 0
+        for i in range(len(lines)):
+            line = lines[i]
+            if (not numspaces) and i>0 and line:
+                numspaces = len(line) - len(line.lstrip())
+            if line.startswith(' '*numspaces):
+                lines[i] = line[numspaces:]
+        doc = '\n'.join(lines)
+
+
+        # Find a formatter to use.
+        # ========================
 
         format = _pre
-        if hasattr(obj, '__docformat__'):
-            if obj.__docformat__ == 'reStructuredText':
-                if _reST:
-                    format = _reST
-        return format(obj.__doc__)
+        for name, obj in reversed(objs):
+            if inspect.ismodule(obj):
+                if hasattr(obj, '__docformat__'):
+                    rest = ('rest', 'restructuredtext')
+                    if obj.__docformat__.lower() in rest:
+                        if _reST:
+                            format = _reST
+
+        # Use it.
+        # =======
+
+        return format(doc)
 
 
 
@@ -175,7 +373,9 @@ TEMPLATE = """\
 
 <body>
 
-<h1>%(crumbs)s</h1>
+<h2>%(crumbs)s</h2>
+
+%(callable)s
 
 <div id="canvas">
 
@@ -184,14 +384,10 @@ TEMPLATE = """\
     </div></div>
 
     <div id="content"><div id="content-hack">
-
-        %(call)s
-
         %(content)s
-
     </div></div>
 
-    <br style="clear: both;" />
+    <div style="clear: both; height: 0px;">&nbsp;</div>
 
 </div>
 
@@ -199,8 +395,8 @@ TEMPLATE = """\
 
 <div id="footer">
 
-    <p>This page was automatically generated from documentation in the source
-    code on %(date)s. This documentation refers to version %(version)s.</p>
+    <p>This document was automatically generated from this object's source code
+    on %(date)s.</p>
 
     <p><a href="http://www.zetadev.com/"> is Zeta software</a>.</p>
 
