cd00c3de58803a945aca0fcabce764fb8e6bd092
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2006-02-09 15:21:22 +0000

    responders

--- httpy/trunk/doc/tex/update.py
+++ httpy/trunk/doc/tex/update.py
@@ -7,26 +7,28 @@ import time
 
 c = 'make'
 mtimes = {}
-while 1:
-    made = False
-    for name in os.listdir('.'):
-        if not (  name == 'Makefile'
-               or name.endswith('.tex')
-               or name.endswith('.css')
-                 ):
-            continue
-        if name not in mtimes:
-            mtime = 0
-        else:
-            mtime = mtimes[name]
-        newtime = os.stat(name)[stat.ST_MTIME]
-        if mtime != newtime:
-            mtimes[name] = newtime
-            if not made:
-                p = subprocess.Popen(c, shell=True)
-                sts = os.waitpid(p.pid, 0)
-                made = True
-            t = time.strftime('%I:%M.%S%p').replace(' 0', ' ')
-            print "%s @ %s" % (name, t)
-    time.sleep(0.5)
-
+try:
+    while 1:
+        made = False
+        for name in os.listdir('.'):
+            if not (  name == 'Makefile'
+                   or name.endswith('.tex')
+                   or name.endswith('.css')
+                     ):
+                continue
+            if name not in mtimes:
+                mtime = 0
+            else:
+                mtime = mtimes[name]
+            newtime = os.stat(name)[stat.ST_MTIME]
+            if mtime != newtime:
+                mtimes[name] = newtime
+                if not made:
+                    p = subprocess.Popen(c, shell=True)
+                    sts = os.waitpid(p.pid, 0)
+                    made = True
+                t = time.strftime('%I:%M.%S%p').replace(' 0', ' ')
+                print "%s @ %s" % (name, t)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    os.system("make clean")
