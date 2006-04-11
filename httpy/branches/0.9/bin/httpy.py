497667bbfbf68c96e2973346c6565f7cde022bc2
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2006-04-11 23:32:18 +0000

    merged trunk r1163:1193 to branches/0.9

--- httpy/branches/0.9/bin/httpy.py
+++ httpy/branches/0.9/bin/httpy.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python
 """httpy -- a straightforward Python webserver. `man 1 httpy' for details.
 """
-from httpy import couple
-from httpy.responders import Multiple
+from httpy.utils import couple
+from httpy.responders.multiple import Responder as Multiple
 
 couple(Multiple())
