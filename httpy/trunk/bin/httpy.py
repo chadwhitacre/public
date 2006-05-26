ce11c6bbd889056ce60d9e5ac89d8f90af650b29
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2006-05-26 20:49:23 +0000

    cruft be gone!

--- httpy/trunk/bin/httpy.py
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/python
-"""httpy -- a straightforward Python webserver. `man 1 httpy' for details.
-"""
-from httpy.utils import couple
-from httpy.responders.multiple import Responder as Multiple
-
-couple(Multiple())
