85e0c4e3965fee9efb45b0951236d94f9349c7d3
Chad Whitacre <chad@zetaweb.com>
2006-06-10 17:14:01 +0000

    refactoring

--- porter/trunk/tests/framework.py
+++
@@ -1,8 +0,0 @@
-""" alter sys.path in case we are installed somewhere besides site-packages """
-import os, sys
-
-cwd = os.getcwd() # this should be tests/
-sep = os.sep
-pkg_parent = sep.join(cwd.split(sep)[:-2])
-if pkg_parent not in sys.path:
-    sys.path.append(pkg_parent)
