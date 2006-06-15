e90ab4430b7b994e4714d89720b1830718d30857
whit537 <whit537@5d61113d-3434-dc49-aa2a-3872fcd73cd9>
2006-06-15 16:37:09 +0000

    moved timeout to configuration, updated docs

--- sanity/trunk/sanity.py
+++ sanity/trunk/sanity.py
@@ -1,24 +1,31 @@
-#!/usr/local/bin/python
-"""see man 1 sanity"""
-
-import dbm, httplib, sys, os
-from popen2 import Popen3
+#!/usr/bin/env python
+"""For usage, see the sanity(1) manual page.
+"""
+import dbm
+import httplib
+import socket
+import sys
+import time
+import os
 from ConfigParser import RawConfigParser, NoOptionError
-from SocketServer import socket # only using socket.error
-from Porter.Porter import Porter # used for sorting domains; that should be a lib
+from popen2 import Popen3
+
 
 class Sanity:
 
     def __init__(self):
 
         self.verbose = sys.argv[1:2] == ['-v']
+        self.VERBOSE = sys.argv[1:2] == ['-V']
+        if self.VERBOSE:
+            self.verbose = True
         self.output = {}
         self.errors = []
 
 
-        ##
-        # read in config info
-        ##
+        # Read in config info.
+        # ====================
+
         CP = RawConfigParser()
         CP.read('/usr/local/etc/sanity.conf')
         self.badconfig = False
@@ -27,6 +34,7 @@ class Sanity:
         if keys <> [ 'email_addy'
                    , 'porter_path'
                    , 'server_ip'
+                   , 'timeout'
                    ]:
             self.badconfig = True
             print "/usr/local/etc/sanity.conf is missing keys"
@@ -34,29 +42,34 @@ class Sanity:
             self.porter_path = CP.get('default', 'porter_path')
             self.server_ip   = CP.get('default', 'server_ip')
             self.email_addy  = CP.get('default', 'email_addy')
+            self.timeout     = float(CP.get('default', 'timeout'))
 
-            ##
-            # sync our porter data and read it in
-            ##
+
+            # Sync our porter data and read it in.
+            # ====================================
+
             os.system('/usr/local/bin/svn cleanup %s' % self.porter_path)
             if self.verbose:
                 sys.stdout.write('syncing dbm ...')
                 os.system('/usr/local/bin/svn up %s' % self.porter_path)
-                if self.verbose: print ' done'
+                if self.verbose:
+                    print ' done'
             else:
                 os.system('/usr/local/bin/svn up --quiet %s' % self.porter_path)
 
-            if self.verbose: sys.stdout.write('reading dbm ...')
-            dbm_path = '%s/var/rewrite' % self.porter_path
+            if self.verbose:
+                sys.stdout.write('reading dbm ...')
+            dbm_path = '%s/rewrite' % self.porter_path
             db = dbm.open(dbm_path, 'r')
             self.rawdata = dict(db)
             db.close()
-            if self.verbose: print ' done'
-
+            if self.verbose:
+                print ' done'
 
 
     def __call__(self):
-        """assuming our config is readable, do our checks"""
+        """Assuming our config is readable, do our checks.
+        """
 
         if not self.badconfig:
             self.check_ping()
@@ -72,9 +85,8 @@ class Sanity:
                     # but we explicitly notify our email address if we have
                     # errors
                     numerrors = len(self.errors); numtoshow = 5
-                    if numerrors == 1: s = ''
-                    else: s = 's'
-                    sub = "%s site%s down" % ( self.output['numerrors'], s)
+                    s = (numerrors != 1) and 's' or ''
+                    sub = "%s site%s down" % (self.output['numerrors'], s)
                     if numerrors > numtoshow:
                         more = ['+ %s more' % (numerrors-numtoshow,)]
                         self.errors = self.errors[:numtoshow] + more
@@ -97,47 +109,131 @@ class Sanity:
         """check websites for HTTP 200, set output accordingly"""
 
         websites = self.rawdata.keys()
-        websites.sort(Porter._domain_cmp)
+        websites.sort(self.domain_cmp)
         websites = [w for w in websites if not w.startswith('www.')]
 
         self.output['numwebsites'] = len(websites)
         self.output['errors'] = self.output['warnings'] = self.output['clear'] = ''
         self.output['numerrors'] = self.output['numwarnings'] = self.output['numclear'] = 0
 
-        if self.verbose: sys.stdout.write('checking sites ')
+        if self.verbose:
+            sys.stdout.write('checking sites ')
 
         for website in websites:
 
-            if self.verbose:
-                sys.stdout.write('.'); sys.stdout.flush()
+            if self.VERBOSE:
+                print website
+            elif self.verbose:
+                sys.stdout.write('.')
+                sys.stdout.flush()
 
-            # try to connect on port 80; if this fails, it means that all of
-            # zetaserver is down and it should fail for all sites
+
+            # Try to connect on port 80.
+            # ==========================
+            # If this fails, it means that all of zetaserver is down and it
+            # should fail for all sites.
+            #
+            #   is this actually true? [cwlw; 2006-06-15]
 
             try:
-                http = httplib.HTTPConnection(website,80)
-                http.connect()
-            except socket.error:
-                self.output['errors'] += 'XXX  %s' % website
-                continue
+                start = time.time()
+                try:
+                    http = httplib.HTTPConnection(website, 80)
 
-            # write output based on site status
-            http.request('GET','/')
-            status = http.getresponse().status
-            line = '%s  %s\n' % (status, website)
-            if status == 200:
-                self.output['numclear'] += 1
-                self.output['clear'] += line
-            elif str(status)[0] == '3':
-                self.output['numwarnings'] += 1
-                self.output['warnings'] += line
-            else:
-                self.output['numerrors'] += 1
-                self.output['errors'] += line
-                self.errors.append(website)
-            http.close()
 
-        if self.verbose: print ' done'
+                    # Monitor the getaddrinfo call.
+                    # =============================
+                    # I'm seeing this behavior: a domain with bad DNS takes
+                    # forever in the next call, but still returns 200
+                    # eventually. I can get the page with fetch, but Firefox
+                    # times out much earlier. We want to flag this condition.
+
+                    res = socket.getaddrinfo( http.host
+                                            , http.port
+                                            , socket.AF_INET
+                                            , socket.SOCK_STREAM
+                                            , socket.IPPROTO_TCP
+                                             )[0]
+                    so_far = time.time() - start
+                    if so_far > self.timeout:
+                        raise socket.error("getaddrinfo returned")
+
+
+                    # Proceed.
+                    # ========
+
+                    af, socktype, proto, canonname, sa = res
+                    http.sock = socket.socket(af, socktype, proto)
+                    http.sock.settimeout(self.timeout)
+                    http.sock.connect(sa)
+
+                    # write output based on site status
+                    http.request('GET','/')
+                    status = http.getresponse().status
+                    line = '%s  %s\n' % (status, website)
+                    if status == 200:
+                        self.output['numclear'] += 1
+                        self.output['clear'] += line
+                    elif str(status)[0] == '3':
+                        self.output['numwarnings'] += 1
+                        self.output['warnings'] += line
+                    else:
+                        self.output['numerrors'] += 1
+                        self.output['errors'] += line
+                        self.errors.append(website)
+
+                except socket.error, msg:
+                    end = time.time()
+                    msg = 'XXX  %s (%s after %d seconds)\n' % ( website
+                                                              , msg
+                                                              , (end-start)
+                                                               )
+                    self.output['errors'] += msg
+                    self.output['numerrors'] += 1
+                    self.errors.append(website)
+
+            finally:
+                http.close()
+
+        if self.verbose:
+            print ' done'
+
+
+    def domain_cmp(self, x, y):
+        """[ripped from porter.py]
+
+        Given two domain names, return -1, 0, or 1
+
+        Domain names must be at least two places long, i.e, example.com, not com
+
+        first sort on SLD (second level domain)
+        then sort on TLD
+        then sort on tertiary
+
+        """
+        # convert to lists, checking for bad data
+        try:    d1 = x.split('.')
+        except: raise PorterError, "unable to parse '%s' as a domain name" % x
+        try:    d2 = y.split('.')
+        except: raise PorterError, "unable to parse '%s' as a domain name" % y
+
+        # more data checks
+        if len(d1) < 2:
+            raise PorterError, "'%s' doesn't look like a full domain name" % x
+        if len(d2) < 2:
+            raise PorterError, "'%s' doesn't look like a full domain name" % y
+
+        # marshall into the order we want for sorting
+        d1tertiary = d1[:-2]; d1tertiary.reverse()
+        d2tertiary = d2[:-2]; d2tertiary.reverse()
+        d1 = [d1[-2], d1[-1], d1tertiary]
+        d2 = [d2[-2], d2[-1], d2tertiary]
+
+        # do the comparison
+        if d1 == d2: return 0
+        if d1 < d2: return -1
+        if d1 > d2: return 1
+
 
     body = """
 
@@ -162,5 +258,5 @@ THE SERVER IS %(server)s
 
 
 if __name__ == '__main__':
-    Sanity = Sanity()
-    Sanity()
+    sanity = Sanity()
+    sanity()
